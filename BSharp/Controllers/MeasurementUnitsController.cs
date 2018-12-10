ffc3089c669e6f7b725df5e6f1175cca2a66828f
Mohamad Akra <mohamad.akra@banan-it.com>
2018-12-10 21:50:57 +0300

    added name uniqueness constraint to Measurement Units

--- BSharp/Controllers/MeasurementUnitsController.cs
+++ BSharp/Controllers/MeasurementUnitsController.cs
@@ -94,15 +94,21 @@ namespace BSharp.Controllers
             var sqlErrors = await _db.Validation.FromSql($@"
 SET NOCOUNT ON;
 DECLARE @ValidationErrors dbo.ValidationErrorList;
-
 	-- Code must be unique
 	INSERT INTO @ValidationErrors([Key], [ErrorName], [Argument1], [Argument2], [Argument3], [Argument4], [Argument5]) 
 	SELECT '[' + CAST(FE.[Index] AS NVARCHAR(255)) + '].Code' As [Key], N'TheCode{{0}}IsUsed' As [ErrorName],
 		FE.Code AS Argument1, NULL AS Argument2, NULL AS Argument3, NULL AS Argument4, NULL AS Argument5
 	FROM @MeasurementUnits FE 
 	JOIN [dbo].MeasurementUnits BE ON FE.Code = BE.Code
-	WHERE (FE.Id IS NULL) OR (FE.Id <> BE.Id);
-	-- Add further logic
+	WHERE (FE.[EntityState] = N'Inserted') OR (FE.Id <> BE.Id);
+	-- Name must be unique
+	INSERT INTO @ValidationErrors([Key], [ErrorName], [Argument1], [Argument2], [Argument3], [Argument4], [Argument5]) 
+	SELECT '[' + CAST(FE.[Index] AS NVARCHAR(255)) + '].Name' As [Key], N'TheName{{0}}IsUsed' As [ErrorName],
+		FE.[Name] AS Argument1, NULL AS Argument2, NULL AS Argument3, NULL AS Argument4, NULL AS Argument5
+	FROM @MeasurementUnits FE 
+	JOIN [dbo].MeasurementUnits BE ON FE.[Name] = BE.[Name]
+	WHERE (FE.[EntityState] = N'Inserted') OR (FE.Id <> BE.Id);
+		-- Add further logic
 
 SELECT TOP {remainingErrorCount} [Key], [ErrorName], [Argument1], [Argument2], [Argument3], [Argument4], [Argument5]
 FROM @ValidationErrors;
