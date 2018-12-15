e1e68f4477de3108803f4dab82c2dd54dcb9eb87
Mohamad Akra <mohamad.akra@banan-it.com>
2018-12-15 12:55:51 +0300

    Measurmeent Units validate now test for duplication in the uploaded list itself

--- BSharp/Controllers/MeasurementUnitsController.cs
+++ BSharp/Controllers/MeasurementUnitsController.cs
@@ -238,6 +238,19 @@ DECLARE @ValidationErrors dbo.ValidationErrorList;
 	FROM @Entities FE 
 	JOIN [dbo].MeasurementUnits BE ON FE.Code = BE.Code
 	WHERE (FE.[EntityState] = N'Inserted') OR (FE.Id <> BE.Id);
+
+	-- Code must not be duplicated in the uploaded list
+	INSERT INTO @ValidationErrors([Key], [ErrorName], [Argument1], [Argument2], [Argument3], [Argument4], [Argument5]) 
+	SELECT '[' + CAST([Index] AS NVARCHAR(255)) + '].Code' As [Key], N'TheCode{{0}}IsUsedInTheList' As [ErrorName],
+		[Code] AS Argument1, NULL AS Argument2, NULL AS Argument3, NULL AS Argument4, NULL AS Argument5
+	FROM @Entities
+	WHERE [Code] IN (
+		SELECT [Code]
+		FROM @Entities
+		GROUP BY [Code]
+		HAVING COUNT(*) > 1
+	)
+
 	-- Name must be unique
 	INSERT INTO @ValidationErrors([Key], [ErrorName], [Argument1], [Argument2], [Argument3], [Argument4], [Argument5]) 
 	SELECT '[' + CAST(FE.[Index] AS NVARCHAR(255)) + '].Name' As [Key], N'TheName{{0}}IsUsed' As [ErrorName],
