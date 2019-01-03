e478573a6ced1518e59e9bbf5bae1ab0f8c24ab5
Ahmad Akra <ahmad.akra@banan-it.com>
2019-01-03 16:37:04 +0300

    Restore point before working on the native mobile app

--- BSharp/ClientApp/src/app/features/measurement-units/measurement-units-import.component.ts
+++ BSharp/ClientApp/src/app/features/measurement-units/measurement-units-import.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'b-measurement-units-import',
   templateUrl: './measurement-units-import.component.html',
-  styleUrls: ['./measurement-units-import.component.css']
 })
 export class MeasurementUnitsImportComponent {
 
