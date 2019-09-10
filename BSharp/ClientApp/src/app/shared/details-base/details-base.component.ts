d2a48f2f7d385815cc524f421d76fec9f72b45b2
Ahmad Akra <ahmad.akra@banan-it.com>
2019-09-10 18:24:12 +0100

    Fixed a few bugs

--- BSharp/ClientApp/src/app/shared/details-base/details-base.component.ts
+++ BSharp/ClientApp/src/app/shared/details-base/details-base.component.ts
@@ -12,9 +12,10 @@ export class DetailsBaseComponent implements ICanDeactivate, OnDestroy {
   // a convenience class that canonical details components can inherit from
   // it provides all common functionality of the TypeScript part of the component
   // and complements a b-details in the root of its HTML template
-
   @Input()
   public mode: 'screen' | 'popup' = 'screen';
+
+  // public mode: 'screen' | 'popup' = 'screen';
 
   @Input()
   public idString: string | number;
