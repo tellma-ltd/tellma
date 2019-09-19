11b77fee46cbb78214e55f3d1e5f8b843ed6d11e
Mohamad Akra <mohamad.akra@banan-it.com>
2019-09-19 13:04:57 +0100

    removed columns fro Resources__Save

--- BSharp.Database.Application/Tests/11_Financing.sql
+++ BSharp.Database.Application/Tests/11_Financing.sql
@@ -37,63 +37,59 @@ BEGIN -- Inserting
 	SELECT * FROM rpt.Documents(@D11Ids) ORDER BY [SortKey], [EntryNumber];
 END
 
-IF (1=0)
-BEGIN -- Updating document and deleting lines/entries
-	INSERT INTO @D12([Index], [Id], [DocumentDate],	[Memo], [EvidenceTypeId])
-	SELECT ROW_NUMBER() OVER(ORDER BY [Id]), [Id], [DocumentDate],	[Memo], [EvidenceTypeId] 
-	FROM dbo.Documents
-	WHERE [DocumentDefinitionId] = N'manual-journals' AND [SerialNumber] = 1;
+--IF (1=0)
+--BEGIN -- Updating document and deleting lines/entries
+--	INSERT INTO @D12([Index], [Id], [DocumentDate],	[Memo], [EvidenceTypeId])
+--	SELECT ROW_NUMBER() OVER(ORDER BY [Id]), [Id], [DocumentDate],	[Memo], [EvidenceTypeId] 
+--	FROM dbo.Documents
+--	WHERE [DocumentDefinitionId] = N'manual-journals' AND [SerialNumber] = 1;
 
-	INSERT INTO @L12([Index], [DocumentIndex],					[Id], [DocumentId], [LineTypeId], [ScalingFactor], [SortKey])
-	SELECT ROW_NUMBER() OVER(ORDER BY DL.[Id]), D12.[Index], DL.[Id], DL.[DocumentId],  DL.[LineTypeId], [ScalingFactor], [SortKey]
-	FROM dbo.DocumentLines DL
-	JOIN @D12 D12 ON D12.[Id] = DL.[DocumentId];
+--	INSERT INTO @L12([Index], [DocumentIndex],					[Id], [DocumentId], [LineTypeId], [ScalingFactor], [SortKey])
+--	SELECT ROW_NUMBER() OVER(ORDER BY DL.[Id]), D12.[Index], DL.[Id], DL.[DocumentId],  DL.[LineTypeId], [ScalingFactor], [SortKey]
+--	FROM dbo.DocumentLines DL
+--	JOIN @D12 D12 ON D12.[Id] = DL.[DocumentId];
 
-	INSERT INTO @E12([Index], [Id], [DocumentLineId], [DocumentIndex], [DocumentLineIndex],				[EntryNumber], [Direction], [AccountId], [IfrsEntryClassificationId], [AgentId], [ResponsibilityCenterId], [ResourceId], [Quantity], [Count], [MonetaryValue], [Value])
-	SELECT ROW_NUMBER() OVER (ORDER BY DLE.[Id]), DLE.[Id], L12.[Id], L12.DocumentIndex, L12.[Index],	[EntryNumber], [Direction], [AccountId], [IfrsEntryClassificationId], [AgentId], [ResponsibilityCenterId], [ResourceId], [Quantity], [Count], [MonetaryValue], [Value]
-	FROM dbo.DocumentLineEntries DLE
-	JOIN @L12 L12 ON L12.[Id] = DLE.[DocumentLineId];
+--	INSERT INTO @E12([Index], [Id], [DocumentLineId], [DocumentIndex], [DocumentLineIndex],				[EntryNumber], [Direction], [AccountId], [IfrsEntryClassificationId], [AgentId], [ResponsibilityCenterId], [ResourceId], [Quantity], [Count], [MonetaryValue], [Value])
+--	SELECT ROW_NUMBER() OVER (ORDER BY DLE.[Id]), DLE.[Id], L12.[Id], L12.DocumentIndex, L12.[Index],	[EntryNumber], [Direction], [AccountId], [IfrsEntryClassificationId], [AgentId], [ResponsibilityCenterId], [ResourceId], [Quantity], [Count], [MonetaryValue], [Value]
+--	FROM dbo.DocumentLineEntries DLE
+--	JOIN @L12 L12 ON L12.[Id] = DLE.[DocumentLineId];
 
-	--SELECT * FROM @D12; SELECT * FROM @L12; SELECT * FROM @E12;
+--	UPDATE @E12 SET [Count] = [Count] / 2, [Value] = [Value] / 2 WHERE [Index] = 1;
+--	UPDATE @E12 SET [Count] = [Count] * 1.5, [Value] = [Value] * 1.5 + 1175000 WHERE [Index] = 2;
+--	UPDATE @L12 SET [ScalingFactor] = 3 WHERE [ScalingFactor] = 1;
+--	DELETE FROM @L12 WHERE [Index] = 1;
+--	DELETE FROM @L12 WHERE [Index] = 3;
 
-	UPDATE @E12 SET [Count] = [Count] / 2, [Value] = [Value] / 2 WHERE [Index] = 1;
-	UPDATE @E12 SET [Count] = [Count] * 1.5, [Value] = [Value] * 1.5 + 1175000 WHERE [Index] = 2;
-	UPDATE @L12 SET [ScalingFactor] = 3 WHERE [ScalingFactor] = 1;
-	DELETE FROM @L12 WHERE [Index] = 1;
-	DELETE FROM @L12 WHERE [Index] = 3;
+--	EXEC [api].[Documents__Save]
+--		@DocumentTypeId = N'manual-journals',
+--		@Documents = @D12, @Lines = @L12, @Entries = @E12,
+--		@ValidationErrorsJson = @ValidationErrorsJson OUTPUT;
 
-	--SELECT * FROM @D12; SELECT * FROM @L12; SELECT * FROM @E12;
+--	IF @ValidationErrorsJson IS NOT NULL 
+--	BEGIN
+--		Print 'Capital Investment (M): Update and Delete'
+--		GOTO Err_Label;
+--	END;
 
-	EXEC [api].[Documents__Save]
-		@DocumentTypeId = N'manual-journals',
-		@Documents = @D12, @Lines = @L12, @Entries = @E12,
-		@ValidationErrorsJson = @ValidationErrorsJson OUTPUT;
+--	INSERT INTO @D12Ids([Id]) SELECT  [Id] FROM dbo.Documents;
+--	SELECT * FROM rpt.Documents(@D12Ids) ORDER BY [SortKey], [EntryNumber];
+--END
 
-	IF @ValidationErrorsJson IS NOT NULL 
-	BEGIN
-		Print 'Capital Investment (M): Update and Delete'
-		GOTO Err_Label;
-	END;
+--BEGIN -- signing
+--	DECLARE @DocsToSign [dbo].[IndexedIdList]
+--	INSERT INTO @DocsToSign([Index], [Id]) SELECT ROW_NUMBER() OVER(ORDER BY [Id]), [Id] FROM dbo.Documents;-- WHERE STATE = N'Draft';
 
-	INSERT INTO @D12Ids([Id]) SELECT  [Id] FROM dbo.Documents;
-	SELECT * FROM rpt.Documents(@D12Ids) ORDER BY [SortKey], [EntryNumber];
-END
+--	EXEC [api].[Documents__Sign]
+--		@DocsIndexedIds = @DocsToSign, @ToState = N'Posted', @ReasonDetails = N'seems ok',
+--		@ValidationErrorsJson = @ValidationErrorsJson OUTPUT;
 
-BEGIN -- signing
-	DECLARE @DocsToSign [dbo].[IndexedIdList]
-	INSERT INTO @DocsToSign([Index], [Id]) SELECT ROW_NUMBER() OVER(ORDER BY [Id]), [Id] FROM dbo.Documents;-- WHERE STATE = N'Draft';
+--	INSERT INTO @D13Ids([Id]) SELECT [Id] FROM dbo.Documents;
+--	SELECT * FROM rpt.Documents(@D13Ids) ORDER BY [SortKey], [EntryNumber];
+--	SELECT * FROM [rpt].[Documents__Signatures](@D13Ids);
 
-	EXEC [api].[Documents__Sign]
-		@DocsIndexedIds = @DocsToSign, @ToState = N'Posted', @ReasonDetails = N'seems ok',
-		@ValidationErrorsJson = @ValidationErrorsJson OUTPUT;
-
-	INSERT INTO @D13Ids([Id]) SELECT [Id] FROM dbo.Documents;
-	SELECT * FROM rpt.Documents(@D13Ids) ORDER BY [SortKey], [EntryNumber];
-	SELECT * FROM [rpt].[Documents__Signatures](@D13Ids);
-
-	IF @ValidationErrorsJson IS NOT NULL 
-	BEGIN
-		Print 'Capital Investment (M): Sign'
-		GOTO Err_Label;
-	END;
-END
+--	IF @ValidationErrorsJson IS NOT NULL 
+--	BEGIN
+--		Print 'Capital Investment (M): Sign'
+--		GOTO Err_Label;
+--	END;
+--END
--- BSharp.Database.Application/dal/Stored Procedures/dal.Resources__Save.sql
+++ BSharp.Database.Application/dal/Stored Procedures/dal.Resources__Save.sql
@@ -1,7 +1,7 @@
 ﻿CREATE PROCEDURE [dal].[Resources__Save]
 	@ResourceDefinitionId NVARCHAR (255),
 	@Resources [dbo].[ResourceList] READONLY,
-	@Picks [dbo].[ResourcePickList] READONLY,
+--	@Picks [dbo].[ResourcePickList] READONLY,
 	@ReturnIds BIT = 0
 AS
 SET NOCOUNT ON;
@@ -21,25 +21,29 @@ SET NOCOUNT ON;
 				[Name], 
 				[Name2], 
 				[Name3], 
-				[UnitMonetaryValue], 
+				--[UnitMonetaryValue], 
+				--[UnitMass], 
+				--[UnitVolume], 
+				--[UnitArea], 
+				--[UnitLength], 
 				[CurrencyId], 
-				[UnitMass], 
+
 				[MassUnitId], 
-				[UnitVolume], 
+
 				[VolumeUnitId],
-				[UnitArea], 
+
 				[AreaUnitId], 
-				[UnitLength], 
+
 				[LengthUnitId], 
-				[UnitTime], 
+				--[UnitTime], 
 				[TimeUnitId], 
-				[UnitCount], 
+				--[UnitCount], 
 				[CountUnitId],
 				[Code], 
-				[SystemCode], 
+				--[SystemCode], 
 				[Memo], 
 				[CustomsReference], 
-				[PreferredSupplierId],
+			--	[PreferredSupplierId],
 				[ResourceLookup1Id], 
 				[ResourceLookup2Id], 
 				[ResourceLookup3Id], 
@@ -53,23 +57,22 @@ SET NOCOUNT ON;
 				t.[Name]					= s.[Name],
 				t.[Name2]					= s.[Name2],
 				t.[Name3]					= s.[Name3],
-				t.[UnitMonetaryValue]		= s.[UnitMonetaryValue],
 				t.[CurrencyId]				= s.[CurrencyId],
-				t.[UnitMass]				= s.[UnitMass],
 				t.[MassUnitId]				= s.[MassUnitId],
-				t.[UnitVolume]				= s.[UnitVolume],
 				t.[VolumeUnitId]			= s.[VolumeUnitId],
 				t.[LengthUnitId]			= s.[LengthUnitId],
 				t.[AreaUnitId]				= s.[AreaUnitId],
-				t.[UnitTime]				= s.[UnitTime],
 				t.[TimeUnitId]				= s.[TimeUnitId],
-				t.[UnitCount]				= s.[UnitCount],
 				t.[CountUnitId]				= s.[CountUnitId],
+				--t.[UnitMonetaryValue]		= s.[UnitMonetaryValue],
+				--t.[UnitMass]				= s.[UnitMass],
+				--t.[UnitVolume]			= s.[UnitVolume],
+				--t.[UnitTime]				= s.[UnitTime],
+				--t.[UnitCount]				= s.[UnitCount],
 				t.[Code]					= s.[Code],
-				t.[SystemCode]				= s.[SystemCode],
+				--t.[SystemCode]				= s.[SystemCode],
 				t.[Memo]					= s.[Memo],      
 				t.[CustomsReference]		= s.[CustomsReference],
-			--	t.[UniversalProductCode]	= s.[UniversalProductCode],
 				t.[PreferredSupplierId]		= s.[PreferredSupplierId],
 				t.[ResourceLookup1Id]		= s.[ResourceLookup1Id],
 				t.[ResourceLookup2Id]		= s.[ResourceLookup2Id],
@@ -79,47 +82,47 @@ SET NOCOUNT ON;
 				t.[ModifiedById]			= @UserId
 		WHEN NOT MATCHED THEN
 			INSERT ([ResourceDefinitionId], [ResourceClassificationId], [Name], [Name2], [Name3],
-				[UnitMonetaryValue], [CurrencyId], [UnitMass], [MassUnitId], [UnitVolume], [VolumeUnitId],
-				[UnitArea], [AreaUnitId], [UnitLength], [LengthUnitId], [UnitTime], [TimeUnitId], [UnitCount], [CountUnitId],
-				[Code], [SystemCode], [Memo], [CustomsReference] , [PreferredSupplierId],
+				[CurrencyId], [MassUnitId], [VolumeUnitId], [AreaUnitId], [LengthUnitId], [TimeUnitId], [CountUnitId],
+				-- [UnitMonetaryValue], [UnitMass], [UnitVolume], [UnitArea], [UnitLength], [UnitTime], [UnitCount], 
+				[Code], [Memo], [CustomsReference] , [PreferredSupplierId],
 				[ResourceLookup1Id], [ResourceLookup2Id], [ResourceLookup3Id], [ResourceLookup4Id])
 			VALUES (@ResourceDefinitionId, s.[ResourceClassificationId], s.[Name], s.[Name2], s.[Name3],
-				s.[UnitMonetaryValue], s.[CurrencyId], s.[UnitMass], s.[MassUnitId], s.[UnitVolume], s.[VolumeUnitId],
-				s.[UnitArea], s.[AreaUnitId], s.[UnitLength], s.[LengthUnitId], s.[UnitTime], s.[TimeUnitId], s.[UnitCount], s.[CountUnitId],
-				s.[Code], s.[SystemCode], s.[Memo], s.[CustomsReference], s.[PreferredSupplierId],
+				s.[CurrencyId], s.[MassUnitId], s.[VolumeUnitId],s.[AreaUnitId],s.[LengthUnitId], s.[TimeUnitId], s.[CountUnitId],
+				-- s.[UnitMonetaryValue], s.[UnitMass], s.[UnitVolume], s.[UnitArea], s.[UnitLength], s.[UnitTime], s.[UnitCount], 
+				s.[Code], s.[Memo], s.[CustomsReference], s.[PreferredSupplierId],
 				s.[ResourceLookup1Id], s.[ResourceLookup2Id], s.[ResourceLookup3Id], s.[ResourceLookup4Id])
 			OUTPUT s.[Index], inserted.[Id]
 	) AS x
 	OPTION (RECOMPILE);
 
-	WITH BE AS (
-		SELECT * FROM dbo.[ResourcePicks]
-		WHERE ResourceId IN (SELECT [Id] FROM @IndexedIds)
-	)
-	MERGE INTO BE AS t
-	USING (
-		SELECT RI.[Id], II.[Id] As ResourceId, RI.[Code], RI.[ProductionDate],
-				[MonetaryValue], [Mass], [Volume], [Area], [Length], [Time], [Count]
-		FROM @Picks RI
-		JOIN @IndexedIds II ON RI.[ResourceIndex] = II.[Index]
-	) AS s
-	ON (s.[Id] = t.[Id])
-	WHEN MATCHED THEN
-		UPDATE SET
-			t.[Code]			= s.[Code],
-			t.[ProductionDate]	= s.[ProductionDate],
-			t.[MonetaryValue]	= s.[MonetaryValue],
-			t.[Mass]			= s.[Mass],
-			t.[Volume]			= s.[Volume],
-			t.[Area]			= s.[Area],
-			t.[Length]			= s.[Length],
-			t.[Time]			= s.[Time],
-			t.[Count]			= s.[Count]
-	WHEN NOT MATCHED THEN
-		INSERT ([ResourceId], [ProductionDate], [Code], [MonetaryValue], [Mass], [Volume], [Area], [Length], [Time], [Count])
-		VALUES(s.[ResourceId], s.[ProductionDate], s.[Code], s.[MonetaryValue], s.[Mass], s.[Volume], s.[Area], s.[Length], s.[Time], s.[Count])
-	WHEN NOT MATCHED BY SOURCE THEN
-		DELETE;
+	--WITH BE AS (
+	--	SELECT * FROM dbo.[ResourcePicks]
+	--	WHERE ResourceId IN (SELECT [Id] FROM @IndexedIds)
+	--)
+	--MERGE INTO BE AS t
+	--USING (
+	--	SELECT RI.[Id], II.[Id] As ResourceId, RI.[Code], RI.[ProductionDate],
+	--			[MonetaryValue], [Mass], [Volume], [Area], [Length], [Time], [Count]
+	--	FROM @Picks RI
+	--	JOIN @IndexedIds II ON RI.[ResourceIndex] = II.[Index]
+	--) AS s
+	--ON (s.[Id] = t.[Id])
+	--WHEN MATCHED THEN
+	--	UPDATE SET
+	--		t.[Code]			= s.[Code],
+	--		t.[ProductionDate]	= s.[ProductionDate],
+	--		t.[MonetaryValue]	= s.[MonetaryValue],
+	--		t.[Mass]			= s.[Mass],
+	--		t.[Volume]			= s.[Volume],
+	--		t.[Area]			= s.[Area],
+	--		t.[Length]			= s.[Length],
+	--		t.[Time]			= s.[Time],
+	--		t.[Count]			= s.[Count]
+	--WHEN NOT MATCHED THEN
+	--	INSERT ([ResourceId], [ProductionDate], [Code], [MonetaryValue], [Mass], [Volume], [Area], [Length], [Time], [Count])
+	--	VALUES(s.[ResourceId], s.[ProductionDate], s.[Code], s.[MonetaryValue], s.[Mass], s.[Volume], s.[Area], s.[Length], s.[Time], s.[Count])
+	--WHEN NOT MATCHED BY SOURCE THEN
+	--	DELETE;
 
 	IF @ReturnIds = 1
 		SELECT * FROM @IndexedIds;
