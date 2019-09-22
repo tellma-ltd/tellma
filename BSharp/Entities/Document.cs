9022f34c17f2256beb911de204e76f9e5c4a3be8
Ahmad Akra <ahmad.akra@banan-it.com>
2019-09-22 17:11:18 +0100

    Restore point before implementing the JV

---
+++ BSharp/Entities/Document.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BSharp.Entities
+{
+    [StrongEntity]
+    public class DocumentForSave : EntityWithKey<int>
+    {
+
+    }
+
+    public class Document : DocumentForSave
+    {
+        public string DocumentDefinitionId { get; set; }
+    }
+}
