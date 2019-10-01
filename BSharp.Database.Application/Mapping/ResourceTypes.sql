839f8837aee8f646021eddfbe7593ff9c95ad91a
Mohamad Akra <mohamad.akra@banan-it.com>
2019-10-01 14:49:58 +0100

    Sync data

---
+++ BSharp.Database.Application/Mapping/ResourceTypes.sql
@@ -0,0 +1,11 @@
+﻿CREATE FUNCTION [map].[ResourceTypes]()
+RETURNS TABLE AS 
+RETURN (
+	SELECT
+		RT.*,
+		(SELECT [Id] FROM dbo.[ResourceTypes] WHERE [Node] = RT.[ParentNode]) AS ParentId,
+		[Node].GetLevel() AS [Level],
+		(SELECT COUNT(*) FROM [dbo].[ResourceTypes] WHERE [IsActive] = 1 AND [Node].IsDescendantOf(RT.[Node]) = 1) As [ActiveChildCount],
+		(SELECT COUNT(*) FROM [dbo].[ResourceTypes] WHERE [Node].IsDescendantOf(RT.[Node]) = 1) As [ChildCount]
+	FROM dbo.[ResourceTypes] RT
+);
