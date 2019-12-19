eaefe72fc2391d0a5b7ba0e5d3c8270a7a2cba03
Mohamad Akra <mohamad.akra@banan-it.com>
2019-12-19 14:49:08 +0300

    Added api.Document__Comment_Save to allow editing the comment to the assignee.

---
+++ BSharp.Database.Application/dal/Stored Procedures/dal.Document_Comment__Save.sql
@@ -0,0 +1,17 @@
+﻿CREATE PROCEDURE [dal].[Document_Comment__Save]
+	@DocumentId INT,
+	@Comment NVARCHAR(1024) = NULL
+AS
+BEGIN
+	DECLARE @UserId INT = CONVERT(INT, SESSION_CONTEXT(N'UserId'));
+	DECLARE @Now DATETIMEOFFSET = SYSDATETIMEOFFSET();
+
+	UPDATE dbo.DocumentAssignments
+	SET Comment = @Comment, [CreatedAt] = @Now, [CreatedById] = @DocumentId
+	WHERE DocumentId = @DocumentId
+
+	INSERT dbo.DocumentAssignmentsHistory([DocumentId], [AssigneeId], [Comment], [CreatedAt], [CreatedById])
+	SELECT [DocumentId], [AssigneeId], [Comment], [CreatedAt], [CreatedById]
+	FROM dbo.DocumentAssignments
+	WHERE DocumentId = @DocumentId;
+END
