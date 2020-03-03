13911e5629b2e8aeb888c1485eefe275d3cdcd66
Ahmad Akra <ahmad.akra@tellma.com>
2020-03-03 19:48:49 +0700

    Updating to Angular v9

--- Tellma/ClientApp/src/polyfills.ts
+++ Tellma/ClientApp/src/polyfills.ts
@@ -57,6 +57,10 @@ import 'classlist.js';
  */
 import 'zone.js/dist/zone';  // Included with Angular CLI.
 
+/******************************************************************
+ * Load `$localize` - used if i18n tags appear in Angular templates.
+ */
+import '@angular/localize/init';
 
 /***************************************************************************************************
  * APPLICATION IMPORTS
