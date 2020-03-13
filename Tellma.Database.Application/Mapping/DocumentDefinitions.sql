cb1856df5012f8e6b4bbb3c771551d808dcf6b14
Mohamad Akra <mohamad.akra@tellma.com>
2020-03-13 18:30:54 +0700

    map.DocumentDefinitions now includes information on available states

--- Tellma.Database.Application/Mapping/DocumentDefinitions.sql
+++ Tellma.Database.Application/Mapping/DocumentDefinitions.sql
@@ -2,5 +2,19 @@
 RETURNS TABLE
 AS
 RETURN (
-	SELECT E.* FROM [dbo].[DocumentDefinitions] E
+	SELECT DD.*, 
+		IIF([1] > 0, 1, 0) AS Has_1,
+		IIF([2] > 1, 1, 0) AS Has_2,
+		IIF([3] > 0, 1, 0) AS Has_3,
+		IIF([4] > 0, 1, 0) AS Has_4
+	FROM dbo.DocumentDefinitions DD
+	LEFT JOIN
+	(
+		SELECT DDLD.DocumentDefinitionId, W.ToState 
+		FROM dbo.DocumentDefinitionLineDefinitions DDLD
+		JOIN dbo.Workflows W ON DDLD.LineDefinitionId = W.LineDefinitionId
+	) AS ST
+	PIVOT  (
+		COUNT([ToState]) FOR [ToState] IN ([1], [2], [3], [4])
+	) AS PT  ON DD.[Id] = PT.DocumentDefinitionId
 );
