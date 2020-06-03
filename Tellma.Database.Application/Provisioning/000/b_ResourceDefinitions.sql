db569b59ba3651b66689313f437a62a8bef13597
Mohamad Akra <mohamad.akra@tellma.com>
2020-06-03 11:46:54 +0700

    Fontawesom fonts are converted to free option

--- Tellma.Database.Application/Provisioning/000/b_ResourceDefinitions.sql
+++ Tellma.Database.Application/Provisioning/000/b_ResourceDefinitions.sql
@@ -1,24 +1,25 @@
 ﻿INSERT INTO @ResourceDefinitions([Index], [Code], [TitleSingular], [TitlePlural], [MainMenuIcon], [MainMenuSection], [MainMenuSortKey]) VALUES
-(0, N'property-plant-equipment', N'Property, plant, and equipment', N'Property, plant, and equipment', N'industry', N'Assets',60),
-(1, N'office-equipment', N'Office equipment', N'Office equipment', N'chair-office', N'Assets',60),
-(2, N'computer-equipment', N'Computer equipment', N'Computer equipment', N'laptop', N'Assets',60),
+(0, N'property-plant-equipment', N'Property, Plant, Equipment', N'Property, plant, and equipment', N'industry', N'Assets',60),
+(1, N'office-equipment', N'Office Equipment', N'Office Equipment', N'fax', N'Assets',60),
+(2, N'computer-equipment', N'Computer Equipment', N'Computer Equipment', N'laptop', N'Assets',60),
 (3, N'machinery', N'Machinery', N'Machineries', N'cogs', N'Assets',70),
 (4, N'vehicles', N'Vehicles', N'Vehicles', N'cars', N'Assets',40),
 (5, N'buildings', N'Building', N'Buildings', N'building', N'Assets',50),
-(6, N'investment-properties', N'Investment property', N'Investment properties', N'city', N'Assets',80),
-(7, N'raw-grains', N'Raw grain', N'Raw grains', N'wheat', N'Purchasing',90),
-(8, N'finished-grains', N'Cleaned grain', N'Cleaned grains', N'wheat', N'Production',100),
-(9, N'byproducts-grains', N'Reject grain', N'Reject grains', N'wheat', N'Production',60),
-(10, N'raw-vehicles', N'Vehicles component', N'Vehicles components', N'tire', N'Purchasing',120),
-(11, N'finished-vehicles', N'Assembled vehicle', N'Assembled vehicles', N'car-side', N'Production',120),
-(12, N'raw-oils', N'Raw materials (Oil Milling)', N'Raw materials (Oil Milling)', N'file-export', N'Purchasing',121),
+(6, N'investment-properties', N'Investment Property', N'Investment Properties', N'city', N'Assets',80),
+(7, N'raw-grains', N'Raw Grain', N'Raw Grains', N'tractor', N'Purchasing',90),
+(8, N'finished-grains', N'Cleaned Grain', N'Cleaned Grains', N'pied-piper-pp', N'Production',100),
+(9, N'byproducts-grains', N'Reject Grain', N'Reject Grains', N'wheat', N'Production',60),
+(10, N'raw-vehicles', N'Vehicles Component', N'Vehicles Components', N'cogs', N'Purchasing',120),
+(11, N'finished-vehicles', N'Assembled Vehicle', N'Assembled Vehicles', N'car-side', N'Production',120),
+(12, N'raw-oils', N'Raw Material (Oil Milling)', N'Raw Materials (Oil Milling)', N'file-export', N'Purchasing',121),
 (13, N'finished-oils', N'Processed Oil (Milling)', N'Processed Oil (Milling)', N'tint', N'Production',122),
-(14, N'byproducts-oils', N'Oil byproduct', N'Oil byproducts', N'tint-slash', N'Production',123),
-(15, N'work-in-progress', N'Work in progress', N'Work in progress', N'spinner', N'Production',124),
+(14, N'byproducts-oils', N'Oil Byproduct', N'Oil Byproducts', N'tint-slash', N'Production',123),
+(15, N'work-in-progress', N'Work in Progress', N'Work In Progress', N'spinner', N'Production',124),
 (16, N'medicines', N'Medicine', N'Medicines', N'pills', N'Purchasing',125),
-(17, N'construction-materials', N'Construction material', N'Construction materials', N'building', N'Purchasing',126),
-(18, N'employee-benefits', N'Employee benefit', N'Employee benefits', N'user-hard-hat', N'HumanCapital',127),
-(19, N'finished-services', N'Revenue service', N'Revenue services', N'servicestack', N'Sales',128);
+(17, N'construction-materials', N'Construction Material', N'Construction Materials', N'building', N'Purchasing',126),
+(18, N'employee-benefits', N'Employee Benefit', N'Employee Benefits', N'user-check', N'HumanCapital',127),
+(19, N'finished-services', N'Revenue Service', N'Revenue Services', N'servicestack', N'Sales',128);
+
 
 	UPDATE @ResourceDefinitions
 	SET 
