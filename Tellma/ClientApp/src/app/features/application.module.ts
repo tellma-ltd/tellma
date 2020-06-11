a8c3959bb8d39199f7bcf854d2781ad7f041a3a7
Ahmad Akra <ahmad.akra@tellma.com>
2020-06-11 09:48:27 +0700

    Restore point before adding GIS support

--- Tellma/ClientApp/src/app/features/application.module.ts
+++ Tellma/ClientApp/src/app/features/application.module.ts
@@ -121,7 +121,6 @@ import { DocumentsPickerComponent } from './documents/documents-picker.component
 import { AgentsDetailsComponent } from './agents/agents-details.component';
 import { AgentsMasterComponent } from './agents/agents-master.component';
 import { AgentsPickerComponent } from './agents/agents-picker.component';
-import { AgmCoreModule } from '@agm/core';
 
 const routes: Routes = [
   {
