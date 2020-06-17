41b4fba2cd6d72e75bf441b7e637778347fdb754
Mohamad Akra <mohamad.akra@tellma.com>
2020-06-17 22:39:38 +0700

    Added more menu icons

--- Tellma.Database.Application/Provisioning/000/p_DocumentDefinitions.sql
+++ Tellma.Database.Application/Provisioning/000/p_DocumentDefinitions.sql
@@ -10,20 +10,20 @@
 (20, N'StockIssueToNonTradingAgent',2, N'Stock issue to production/maintenance/job/Consumption/Reclassification',N'Stock Issue Voucher (NT)', N'Stock Issue Vouchers (NT)', N'MIO',NULL, N'Inventory', 1120),
 (21, N'StockTransfer',2, N'transfer between warehouses',N'Stock Transfer', N'Stock Transfers (NT)', N'MTV',N'dolly-flatbed', N'Inventory', 1130),
 (22, N'StockReceiptFromNonTradingAgent',2, N'FG receipt from production, RM/production supplies return from production/maintenance/job/consumption/Reclassification',N'Stock Receipt Voucher (NT)', N'Stock Receipt Voucher (NT)', N'MRO',NULL, N'Inventory', 1140),
-(23, N'InventoryAdjustment',2, N'Shortage, Overage, impairment, reversal of impairment',N'Inventory Adjustment', N'Inventory Adjustments', N'MAV',NULL, N'Inventory', 1150),
+(23, N'InventoryAdjustment',2, N'Shortage, Overage, impairment, reversal of impairment',N'Inventory Adjustment', N'Inventory Adjustments', N'MAV',N'edit', N'Inventory', 1150),
 
 (30, N'PaymentIssueToTradePayable',2, N'payment to supplier, purchase invoice, stock/PPE/C/S receipt from supplier',N'Cash Payment (Supplier)', N'Cash Payments (Supplier)', N'PIS',N'money-check-alt', N'Purchasing', 1170),
 (31, N'RefundFromTradePayable',2, N'refund from supplier, credit note (supplier), stock return to supplier, ppe return to supplier',N'Supplier Refund Voucher', N'Suppliers Refund Vouchers', N'PRS',NULL, N'Purchasing', 1180),
-(32, N'WithholdingTaxFromTradePayable',2, N'Witholding tax from suppliers/lessors',N'WT (Supplier)', N'WT (Suppliers)', N'WTS',NULL, N'Purchasing', 1190),
-(33, N'ImportFromTradePayable',2, N'Shipment In Transit, Payment, Commercial Invoice, Related Expenses',N'Import Shipment', N'Import Shipments', N'IRS',NULL, N'Purchasing', 1200),
+(32, N'WithholdingTaxFromTradePayable',2, N'Witholding tax from suppliers/lessors',N'WT (Supplier)', N'WT (Suppliers)', N'WTS',N'folder-minus', N'Purchasing', 1190),
+(33, N'ImportFromTradePayable',2, N'Shipment In Transit, Payment, Commercial Invoice, Related Expenses',N'Import Shipment', N'Import Shipments', N'IRS',N'pallet', N'Purchasing', 1200),
 (34, N'GoodReceiptFromImport',2, N'goods receipt from import (PPE treated as stock till mise in use)',N'Good Receipt (Import)', N'Goods Receipts (Import)', N'GRI',NULL, N'Purchasing', 1210),
 (35, N'GoodServiceReceiptFromTradePayable',2, N'PPE/consumables/services/rental receipt from supplier, purchase invoice, debit note (supplier)',N'Good/Service Receipt (Purchase)', N'Goods/Services Receipts (Purchase)', N'GSRS',NULL, N'Purchasing', 1220),
 
 (40, N'PaymentReceiptFromTradeReceivable',2, N'payment from customer, sales invoice, Goods/Service issue to customer',N'Cash Receipt (Customer)', N'Cash Receipts (Customers)', N'PRC',NULL, N'Sales', 1240),
 (41, N'RefundToTradeReceivable',2, N'payment to customer, credit note (customer), stock receipt from customer',N'Customer Refund', N'Customer Refunds', N'PIC',NULL, N'Sales', 1250),
-(42, N'WithholdingTaxByTradeReceivable',2, N'Witholding tax by customers/lessees',N'WT (Customer)', N'WT (Customers)', N'WTC',NULL, N'Sales', 1260),
+(42, N'WithholdingTaxByTradeReceivable',2, N'Witholding tax by customers/lessees',N'WT (Customer)', N'WT (Customers)', N'WTC',N'folder-plus', N'Sales', 1260),
 (43, N'GoodIssueToExport',2, N'goods issue to export, payment, sales invoice, FOB destination',N'Export Shipment', N'Goods Issues (Exports)', N'GIE',NULL, N'Sales', 1270),
-(44, N'ExportToTradeReceivable',2, N'goods delivery from export',N'Goods Delivery (Export)', N'Goods Deliveries (Exports)', N'EIC',NULL, N'Sales', 1280),
+(44, N'ExportToTradeReceivable',2, N'goods delivery from export',N'Goods Delivery (Export)', N'Goods Deliveries (Exports)', N'EIC',N'ship', N'Sales', 1280),
 (45, N'GoodServiceIssueToTradeReceivable',2, N'stock/rental/service issue to customer, sales invoice, debit note (customer)',N'Good/Service Issue (Customer)', N'Goods/Services Issue (Customer)', N'GSIC',NULL, N'Sales', 1290),
 
 (50, N'SteelProduction',2, N'DM/DL/OH to WIP/Byproduct, DM/DL/OH + WIP to WIP/Byproduct, DM/DL/OH + WIP to FG/Byproduct',N'Steel Production Voucher', N'Steel Production Vouchers', N'PV1',NULL, N'Production', 1310),
@@ -35,15 +35,15 @@
 
 (69, N'Maintenance',2, N'DM/DL/OH to Job, then total allocated to machine',N'Internal Maintenance Job', N'Internal Maintenance Jobs', N'IMJ',NULL, N'Production', 1380),
 
-(70, N'PaymentIssueToEmployee',2, N'payment - employee benefits, payment - employee loan, salary, overtime, absence, deduction, due installments, Bonus',N'Cash Payment', N'Cash Payments', N'PIE',NULL, N'HumanCapital', 1400),
-(71, N'EmployeeLoan',2, N'salary advance, long term loan, loan installments',N'Employee Loan Voucher', N'', N'ELN',NULL, N'HumanCapital', 1410),
-(72, N'AttendanceRegister',2, N'arrivals, departures',N'Attendance Register', N'Attendance Register', N'SRE',NULL, N'HumanCapital', 1420),
-(73, N'EmployeeOvertime',2, N'Overtime (Employee)',N'Overtime', N'Overtime', N'ORE',NULL, N'HumanCapital', 1430),
-(74, N'EmployeePenalty',2, N'absence penalty, Other penalties',N'Penalty', N'Penalties', N'PTE',NULL, N'HumanCapital', 1440),
-(75, N'EmployeeReward',2, N'periodic bonus, special bonus',N'Reward', N'Rewards', N'RTE',NULL, N'HumanCapital', 1450),
-(76, N'EmployeeLeave',2, N'Paid leave, Unpaid leave, hourly leave',N'Leave', N'Leaves', N'LIE',NULL, N'HumanCapital', 1460),
-(77, N'EmployeeLeaveAllowance',2, N'Yearly Leave',N'Leave Allowance', N'Leave Allowances', N'LAE',NULL, N'HumanCapital', 1470),
-(78, N'EmployeeTravel',2, N'Per diem, Petty Cash, Fuel Allowance, ...',N'Travel', N'Travels', N'TIE',NULL, N'HumanCapital', 1480);
+(70, N'PaymentIssueToEmployee',2, N'payment - employee benefits, payment - employee loan, salary, overtime, absence, deduction, due installments, Bonus',N'Cash Payment', N'Cash Payments', N'PIE',N'hand-holding-usd', N'HumanCapital', 1400),
+(71, N'EmployeeLoan',2, N'salary advance, long term loan, loan installments',N'Employee Loan Voucher', N'', N'ELN',N'tasks', N'HumanCapital', 1410),
+(72, N'AttendanceRegister',2, N'arrivals, departures',N'Attendance Register', N'Attendance Register', N'SRE',N'user-clock', N'HumanCapital', 1420),
+(73, N'EmployeeOvertime',2, N'Overtime (Employee)',N'Overtime', N'Overtime', N'ORE',N'user-plus', N'HumanCapital', 1430),
+(74, N'EmployeePenalty',2, N'absence penalty, Other penalties',N'Penalty', N'Penalties', N'PTE',N'user-minus', N'HumanCapital', 1440),
+(75, N'EmployeeReward',2, N'periodic bonus, special bonus',N'Reward', N'Rewards', N'RTE',N'trophy', N'HumanCapital', 1450),
+(76, N'EmployeeLeave',2, N'Paid leave, Unpaid leave, hourly leave',N'Leave', N'Leaves', N'LIE',N'spa', N'HumanCapital', 1460),
+(77, N'EmployeeLeaveAllowance',2, N'Yearly Leave',N'Leave Allowance', N'Leave Allowances', N'LAE',N'umbrella-beach', N'HumanCapital', 1470),
+(78, N'EmployeeTravel',2, N'Per diem, Petty Cash, Fuel Allowance, ...',N'Travel', N'Travels', N'TIE',N'suitcase-rolling', N'HumanCapital', 1480);
 INSERT @DocumentDefinitionLineDefinitions([Index], [HeaderIndex], [LineDefinitionId], [IsVisibleByDefault]) VALUES
 (0,0, @ManualLineLD, 1),
 (0,10, @CashPaymentToOtherLD, 1),
