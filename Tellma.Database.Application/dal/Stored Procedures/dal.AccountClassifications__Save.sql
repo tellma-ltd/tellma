4e3d1baea6ae531a6161cb0a59e0f65ab18d775f
Mohamad Akra <mohamad.akra@tellma.com>
2020-06-19 10:42:03 +0700

    Added Accounts Provisioning data

--- Tellma.Database.Application/dal/Stored Procedures/dal.AccountClassifications__Save.sql
+++ Tellma.Database.Application/dal/Stored Procedures/dal.AccountClassifications__Save.sql
@@ -76,25 +76,25 @@ SET NOCOUNT ON;
 */
 
 
-	--WITH Children ([Id], [ParentId], [Num]) AS (
-	--	SELECT E.[Id], E2.[Id] As ParentId, ROW_NUMBER() OVER (PARTITION BY E2.[Id] ORDER BY E2.[Id])
-	--	FROM [dbo].[AccountClassifications] E
-	--	LEFT JOIN [dbo].[AccountClassifications] E2 ON E.[ParentId] = E2.[Id]
-	--),
-	--Paths ([Node], [Id]) AS (  
-	--	-- This section provides the value for the roots of the hierarchy  
-	--	SELECT CAST(('/'  + CAST(C.Num AS VARCHAR(30)) + '/') AS HIERARCHYID) AS [Node], [Id]
-	--	FROM Children AS C   
-	--	WHERE [ParentId] IS NULL
-	--	UNION ALL   
-	--	-- This section provides values for all nodes except the root  
-	--	SELECT CAST(P.[Node].ToString() + CAST(C.Num AS VARCHAR(30)) + '/' AS HIERARCHYID), C.[Id]
-	--	FROM Children C
-	--	JOIN Paths P ON C.[ParentId] = P.[Id]
-	--)
-	--MERGE INTO [dbo].[AccountClassifications] As t
-	--USING Paths As s ON (t.[Id] = s.[Id])
-	--WHEN MATCHED THEN UPDATE SET t.[Node] = s.[Node];
+	WITH Children ([Id], [ParentId], [Num]) AS (
+		SELECT E.[Id], E2.[Id] As ParentId, ROW_NUMBER() OVER (PARTITION BY E2.[Id] ORDER BY E2.[Id])
+		FROM [dbo].[AccountClassifications] E
+		LEFT JOIN [dbo].[AccountClassifications] E2 ON E.[ParentId] = E2.[Id]
+	),
+	Paths ([Node], [Id]) AS (  
+		-- This section provides the value for the roots of the hierarchy  
+		SELECT CAST(('/'  + CAST(C.Num AS VARCHAR(30)) + '/') AS HIERARCHYID) AS [Node], [Id]
+		FROM Children AS C   
+		WHERE [ParentId] IS NULL
+		UNION ALL   
+		-- This section provides values for all nodes except the root  
+		SELECT CAST(P.[Node].ToString() + CAST(C.Num AS VARCHAR(30)) + '/' AS HIERARCHYID), C.[Id]
+		FROM Children C
+		JOIN Paths P ON C.[ParentId] = P.[Id]
+	)
+	MERGE INTO [dbo].[AccountClassifications] As t
+	USING Paths As s ON (t.[Id] = s.[Id])
+	WHEN MATCHED THEN UPDATE SET t.[Node] = s.[Node];
 
 	IF @ReturnIds = 1
 		SELECT * FROM @IndexedIds;
