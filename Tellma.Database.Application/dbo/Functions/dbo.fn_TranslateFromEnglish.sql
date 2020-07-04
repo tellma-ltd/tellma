839ec59be298869cf162cf3a94401e859c7eb3e8
Mohamad Akra <mohamad.akra@tellma.com>
2020-07-04 18:39:42 +0700

    Improved provision data

--- Tellma.Database.Application/dbo/Functions/dbo.fn_TranslateFromEnglish.sql
+++ Tellma.Database.Application/dbo/Functions/dbo.fn_TranslateFromEnglish.sql
@@ -1,11 +1,11 @@
 ﻿CREATE FUNCTION [dbo].[fn_TranslateFromEnglish]
 (
 	@TableName NVARCHAR(50),
-	@SourceEnglishWord NVARCHAR (100),
+	@SourceEnglishWord NVARCHAR (1024),
 	@DestinationCultureId NVARCHAR(5),
 	@Form NCHAR (1)
 )
-RETURNS NVARCHAR (100)
+RETURNS NVARCHAR (1024)
 AS BEGIN
 	IF NOT EXISTS(
 		SELECT * FROM dbo.Translations
