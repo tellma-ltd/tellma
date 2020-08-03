3e1425bddded230be71d94c64aa3e94077340310
Ahmad Akra <ahmad.akra@tellma.com>
2020-08-03 09:18:27 +0700

    Working on line definitions API

---
+++ Tellma.Database.Application/bll/Stored Procedures/bll.LineDefinitions_Validate__Delete.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE [bll].[LineDefinitions_Validate__Delete]
+	@Ids [dbo].[IndexedIdList] READONLY,
+	@Top INT = 10
+AS
+SET NOCOUNT ON;
+	DECLARE @ValidationErrors [dbo].[ValidationErrorList], @UserId INT = CONVERT(INT, SESSION_CONTEXT(N'UserId'));
+
+	-- TODO
+
+	SELECT TOP (@Top) * FROM @ValidationErrors;
---
+++ Tellma.Database.Application/dal/Stored Procedures/dal.LineDefinitions__Delete.sql
@@ -0,0 +1,8 @@
+﻿CREATE PROCEDURE [dal].[LineDefinitions__Delete]
+	@Ids [dbo].[IdList] READONLY
+AS
+	DELETE FROM [dbo].[LineDefinitions] 
+	WHERE Id IN (SELECT Id FROM @Ids);
+	
+	-- Signal clients to refresh their cache
+	UPDATE [dbo].[Settings] SET [DefinitionsVersion] = NEWID()
