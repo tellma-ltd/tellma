027a5a2e7def8f1be6015ed9bc5e9ae4aa8d162a
Ahmad Akra <ahmad.akra@tellma.com>
2020-08-03 11:42:57 +0700

    Cascade delete directory user memberships

--- Tellma.Database.Manager/dbo/Tables/DirectoryUserMemberships.sql
+++ Tellma.Database.Manager/dbo/Tables/DirectoryUserMemberships.sql
@@ -4,5 +4,5 @@
     [DatabaseId] INT NOT NULL, 
 	PRIMARY KEY ([UserId], [DatabaseId]),
     CONSTRAINT [FK_DirectoryUserMemberships_DirectoryUsers] FOREIGN KEY ([UserId]) REFERENCES [DirectoryUsers]([Id]),
-    CONSTRAINT [FK_DirectoryUserMemberships_SqlDatabases] FOREIGN KEY ([DatabaseId]) REFERENCES [SqlDatabases]([Id])
+    CONSTRAINT [FK_DirectoryUserMemberships_SqlDatabases] FOREIGN KEY ([DatabaseId]) REFERENCES [SqlDatabases]([Id]) ON UPDATE CASCADE ON DELETE CASCADE
 )
