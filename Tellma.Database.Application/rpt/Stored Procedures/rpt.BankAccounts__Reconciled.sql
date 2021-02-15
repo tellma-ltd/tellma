34e588ee4f70288ada6972c8859f7b48b5a116a9
Mohamad Akra <mohamad.akra@tellma.com>
2021-02-15 07:26:01 +0300

    Added Reports

---
+++ Tellma.Database.Application/rpt/Stored Procedures/rpt.BankAccounts__Reconciled.sql
@@ -0,0 +1,21 @@
+﻿CREATE PROCEDURE [rpt].[BankAccounts__Reconciled]
+	@AsOfDate DATE
+AS
+	DECLARE @ReconciliationIds IdList;
+
+	WITH BankAccounts AS (
+		SELECT [Id] FROM dbo.Accounts
+		WHERE AccountTypeId IN (
+			SELECT [Id] FROM dbo.AccountTypes
+			WHERE [Concept] = N'BalancesWithBanks'
+		)
+	)
+	SELECT C.[Name] AS BankAccount,
+		ReconciledCount = COUNT(DISTINCT R.[Id])
+	FROM dbo.Reconciliations  R
+	JOIN dbo.ReconciliationExternalEntries REE ON R.[Id] = REE.ReconciliationId
+	JOIN dbo.ExternalEntries EE ON REE.[ExternalEntryId] = EE.[Id]
+	JOIN dbo.Custodies C ON EE.[CustodyId] = C.[Id]
+	WHERE EE.[AccountId] IN (SELECT [Id] FROM BankAccounts)
+	AND (@AsOfDate IS NULL OR PostingDate >= @AsOfDate)
+	GROUP BY EE.[CustodyId], C.[Name];
---
+++ Tellma.Database.Application/rpt/Stored Procedures/rpt.RentalSpaces__Areas.sql
@@ -0,0 +1,21 @@
+﻿CREATE PROCEDURE [rpt].[RentalSpaces__Areas]
+@AsOfDate DATE = N'2021.02.7'
+AS
+	SELECT LK.[Code], R.[Code],
+		SUM(R.Decimal1) AS Area,
+		SUM(IIF(T.ResourceId IS NOT NULL, R.[Decimal1], NULL)) AS RentedArea
+	FROM dbo.Resources R
+	JOIN dbo.Lookups LK ON R.[Lookup2Id] = LK.[Id]
+	JOIN dbo.ResourceDefinitions RD ON R.[DefinitionId] = RD.[Id]
+	LEFT JOIN (
+		SELECT E.[ResourceId] 
+		FROM dbo.Entries E
+		JOIN dbo.Lines L ON L.[Id] = E.[LineId]
+		JOIN dbo.Relations RL ON E.[ParticipantId] = RL.[Id]
+		JOIN dbo.Lookups LK2 ON RL.[Lookup1Id] = LK2.[Id]
+		WHERE E.Time1 <= @AsOfDate AND E.Time2 >= @AsOfDate
+		AND LK2.[Code] = N'R'
+		AND L.[State] = 2
+	) T ON R.[Id] = T.[ResourceId]
+	WHERE RD.[Code] = N'RentalSpace'
+	GROUP BY ROLLUP( LK.[Code], R.[Code])
