fb9b6be1d4e2362674d389c888b4b443e18c64a8
Mohamad Akra <mohamad.akra@tellma.com>
2021-04-20 10:14:54 +0300

    reverting other reconciliation logic

--- Tellma.Database.Application/dal/Stored Procedures/dal.Reconciliation__Load_Unreconciled.sql
+++ Tellma.Database.Application/dal/Stored Procedures/dal.Reconciliation__Load_Unreconciled.sql
@@ -11,7 +11,6 @@
 	@UnreconciledExternalEntriesBalance	DECIMAL (19,4) OUTPUT,
 	@UnreconciledEntriesCount			INT OUTPUT,
 	@UnreconciledExternalEntriesCount	INT OUTPUT
-WITH RECOMPILE
 AS
 	SELECT @EntriesBalance = SUM(E.[Direction] * E.[MonetaryValue])
 	FROM dbo.Entries E
@@ -21,94 +20,64 @@ AS
 	AND L.[State] = 4
 	AND L.[PostingDate] <= @AsOfDate;
 
-	--With SpecialEntries AS ( -- Internal Entries that were reconciled @AsOfDate
-	--	SELECT DISTINCT RE.[EntryId]
-	--	FROM dbo.ReconciliationEntries RE
-	--	JOIN dbo.Reconciliations R ON RE.ReconciliationId = R.Id
-	--	JOIN dbo.ReconciliationExternalEntries REE ON REE.ReconciliationId = R.Id
-	--	JOIN dbo.ExternalEntries EE ON REE.ExternalEntryId = EE.Id
-	--	WHERE EE.PostingDate <=  @AsOfDate	
-	--	AND EE.[AccountId] = @AccountId
-	--	AND EE.[CustodyId] = @CustodyId
-	--)
-	SELECT
-		@UnreconciledEntriesCount = COUNT(*),
-		@UnreconciledEntriesBalance = SUM( E.[Direction] * E.[MonetaryValue])
-	FROM dbo.Entries E
-	JOIN dbo.Lines L ON E.[LineId] = L.[Id]
-	WHERE E.[CustodyId] = @CustodyId
-	AND E.[AccountId] = @AccountId
-	AND L.[State] = 4
-	AND L.[PostingDate] <= @AsOfDate
-	AND E.[Id] NOT IN (
+	With SpecialEntries AS ( -- Internal Entries that were reconciled @AsOfDate
 		SELECT DISTINCT RE.[EntryId]
 		FROM dbo.ReconciliationEntries RE
 		JOIN dbo.Reconciliations R ON RE.ReconciliationId = R.Id
 		JOIN dbo.ReconciliationExternalEntries REE ON REE.ReconciliationId = R.Id
 		JOIN dbo.ExternalEntries EE ON REE.ExternalEntryId = EE.Id
-		WHERE (EE.PostingDate <=  @AsOfDate)
-		AND (EE.[AccountId] = @AccountId)
-		AND (EE.[CustodyId] = @CustodyId)
+		WHERE EE.PostingDate <=  @AsOfDate	
+		AND EE.[AccountId] = @AccountId
+		AND EE.[CustodyId] = @CustodyId
 	)
+	SELECT
+		@UnreconciledEntriesCount = COUNT(*),
+		@UnreconciledEntriesBalance = SUM(E.[Direction] * E.[MonetaryValue])
+	FROM dbo.Entries E
+	JOIN dbo.Lines L ON E.[LineId] = L.[Id]
+	WHERE E.[CustodyId] = @CustodyId
+	AND E.[AccountId] = @AccountId
+	AND L.[State] = 4
+	AND (
+		L.[PostingDate] > @AsOfDate -- was reconciled after @AsOfDate
+		AND E.[Id] IN (SELECT EntryId FROM SpecialEntries)
+		OR
+		L.[PostingDate] <= @AsOfDate   -- or was not reconciled @AsOfDate
+		AND E.[Id] NOT IN (SELECT EntryId FROM SpecialEntries)
+	);
 
-	SELECT @UnreconciledExternalEntriesCount = COUNT(*),
-		@UnreconciledExternalEntriesBalance = SUM(E.[Direction] * E.[MonetaryValue])
+	SELECT @UnreconciledExternalEntriesCount = COUNT(*), @UnreconciledExternalEntriesBalance = SUM(E.[Direction] * E.[MonetaryValue])
 	FROM dbo.ExternalEntries E
 	WHERE E.[CustodyId] = @CustodyId
 	AND E.[AccountId] = @AccountId
+	AND E.[Id] NOT IN (SELECT [ExternalEntryId] FROM dbo.ReconciliationExternalEntries)
 	AND E.[PostingDate] <= @AsOfDate
-	AND E.[Id] NOT IN (
-		SELECT DISTINCT REE.[ExternalEntryId]
-		FROM dbo.ReconciliationExternalEntries REE
-		JOIN dbo.Reconciliations R ON REE.ReconciliationId = R.Id
-		JOIN dbo.ReconciliationEntries RE ON RE.ReconciliationId = R.Id
-		JOIN dbo.Entries E ON RE.EntryId = E.Id
-		JOIN dbo.Lines L ON E.LineId = L.Id
-		WHERE (L.PostingDate <= @AsOfDate)
-		AND (E.[AccountId] = @AccountId)
-		AND (E.[CustodyId] = @CustodyId)
-	)
 	
 	SELECT E.[Id], L.[PostingDate], E.[Direction], E.[MonetaryValue],
 		IIF([Direction] = 1, E.[NotedAgentName], E.[InternalReference]) AS ExternalReference,
 		L.[DocumentId], D.[DefinitionId] AS [DocumentDefinitionId], D.[SerialNumber] AS [DocumentSerialNumber],
-		CAST(IIF(E.[Id] IN (SELECT [EntryId] FROM dbo.ReconciliationEntries), 1, 0) AS BIT) AS IsReconciledLater
+			CAST(IIF(E.[Id] IN (SELECT [EntryId] FROM dbo.ReconciliationEntries), 1, 0) AS BIT) AS IsReconciledLater
 	FROM dbo.Entries E
 	JOIN dbo.Lines L ON E.[LineId] = L.[Id]
 	JOIN dbo.Documents D ON L.[DocumentId] = D.[Id]
 	WHERE E.[CustodyId] = @CustodyId
 	AND E.[AccountId] = @AccountId
 	AND L.[State] = 4
-	AND L.[PostingDate] <= @AsOfDate
 	AND E.[Id] NOT IN (
-		SELECT DISTINCT RE.[EntryId]
-		FROM dbo.ReconciliationEntries RE
-		JOIN dbo.Reconciliations R ON RE.ReconciliationId = R.Id
-		JOIN dbo.ReconciliationExternalEntries REE ON REE.ReconciliationId = R.Id
-		JOIN dbo.ExternalEntries EE ON REE.ExternalEntryId = EE.Id
-		WHERE (EE.PostingDate <=  @AsOfDate)
-		AND (EE.[AccountId] = @AccountId)
-		AND (EE.[CustodyId] = @CustodyId)
+		SELECT [EntryId] FROM dbo.ReconciliationEntries
 	)
+	AND L.[PostingDate] <= @AsOfDate
 	ORDER BY L.[PostingDate], E.[MonetaryValue], E.[ExternalReference]
 	OFFSET (@Skip) ROWS FETCH NEXT (@Top) ROWS ONLY;
 	
-	SELECT E.[Id], E.[PostingDate], E.[Direction], E.[MonetaryValue], E.[ExternalReference], E.[CreatedById], E.[CreatedAt], E.[ModifiedById], E.[ModifiedAt],
-	CAST(IIF(E.[Id] IN (SELECT [ExternalEntryId] FROM dbo.ReconciliationExternalEntries), 1, 0) AS BIT) AS IsReconciledLater
+	SELECT E.[Id], E.[PostingDate], E.[Direction], E.[MonetaryValue],
+		E.[ExternalReference],
+		E.[CreatedById], E.[CreatedAt], E.[ModifiedById], E.[ModifiedAt],
+		CAST(IIF(E.[Id] IN (SELECT [EntryId] FROM dbo.ReconciliationEntries), 1, 0) AS BIT) AS IsReconciledLater
 	FROM dbo.ExternalEntries E
 	WHERE E.[CustodyId] = @CustodyId
 	AND E.[AccountId] = @AccountId
+	AND E.[Id] NOT IN (SELECT [ExternalEntryId] FROM dbo.ReconciliationExternalEntries)
 	AND E.[PostingDate] <= @AsOfDate
-	AND E.[Id] NOT IN (
-		SELECT DISTINCT REE.[ExternalEntryId]
-		FROM dbo.ReconciliationExternalEntries REE
-		JOIN dbo.Reconciliations R ON REE.ReconciliationId = R.Id
-		JOIN dbo.ReconciliationEntries RE ON RE.ReconciliationId = R.Id
-		JOIN dbo.Entries E ON RE.EntryId = E.Id
-		JOIN dbo.Lines L ON E.LineId = L.Id
-		WHERE (L.PostingDate <= @AsOfDate)
-		AND (E.[AccountId] = @AccountId)
-		AND (E.[CustodyId] = @CustodyId)
-	)
 	ORDER BY E.[PostingDate], E.[MonetaryValue], E.[ExternalReference]
 	OFFSET (@SkipExternal) ROWS FETCH NEXT (@TopExternal) ROWS ONLY;
