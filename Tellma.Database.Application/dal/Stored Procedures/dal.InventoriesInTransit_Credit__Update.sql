e919144d942ffc1341daba222aa2389f82812f6c
Mohamad Akra <mohamad.akra@tellma.com>
2021-05-30 09:42:28 +0300

    [dal].[InventoriesInTransit_Credit__Update] & [dal].[InventoryEntries_AVCO__Update]

---
+++ Tellma.Database.Application/dal/Stored Procedures/dal.InventoriesInTransit_Credit__Update.sql
@@ -0,0 +1,16 @@
+﻿CREATE PROCEDURE [dal].[InventoriesInTransit_Credit__Update]
+@CustodyId INT
+AS
+-- For each LC center (also duplicated as custody)
+-- For each resource
+--	Sum all the Quantities debited to IIT account (increase/decrease through other changes)
+--	Sum all the values debited to IIT account (either directly or by re-assignment from Expenses)
+--	Compute Sum of Value / Sum of Quantity
+-- For each LD involving IIT
+--	Update the credit entry with the new average price
+--	Update the related debit entry as well. 
+SELECT SUM(E.[Quantity]) AS TotalQuantity, SUM(MonetaryValue) AS TotalMonetaryValue, SUM([Value]) AS TotalValue
+FROM map.DetailsEntries() E
+JOIN dbo.Lines L ON L.[Id] = E.[LineId]
+WHERE L.[State] = 4
+AND (@CustodyId IS NULL OR [CustodyId] = @CustodyId)
--- Tellma.Database.Application/dal/Stored Procedures/dal.InventoryEntries_AVCO__Update.sql
+++ Tellma.Database.Application/dal/Stored Procedures/dal.InventoryEntries_AVCO__Update.sql
@@ -1,5 +1,5 @@
 ﻿CREATE PROCEDURE [dal].[InventoryEntries_AVCO__Update] -- [dal].[InventoryEntries_AVCO__Update] 0
-@VerifyLineDefinitions BIT = 1
+@VerifyLineDefinitions BIT = 0 -- Bug with value = 1
 AS
 	DECLARE @AffectedLineDefinitionEntries TABLE (
 		[LineDefinitionId] INT,
@@ -8,12 +8,12 @@ AS
 	);
 
 	DECLARE @T TABLE (
-		[Id]					INT PRIMARY KEY IDENTITY,
-		[PostingDate]			DATE,
-		[LineId]				INT,
-		[Direction]				SMALLINT,
+		[Id]					INT PRIMARY KEY IDENTITY,	
+		[AccountId]				INT,
 		[CustodyId]				INT,
 		[ResourceId]			INT,
+		[PostingDate]			DATE,
+		[Direction]				SMALLINT,
 		[AlgebraicQuantity]		DECIMAL (19, 4),
 		[AlgebraicMonetaryValue]DECIMAL (19, 4),
 		[AlgebraicValue]		DECIMAL (19, 4),
@@ -25,42 +25,49 @@ AS
 	);
 
 	DECLARE @BadLineDefinitionId INT;
-	DECLARE @ManualLine INT = (SELECT [Id] FROM dbo.LineDefinitions WHERE [Code] = N'ManualLine');
+	DECLARE @ManualLineLD INT = (SELECT [Id] FROM dbo.LineDefinitions WHERE [Code] = N'ManualLine');
 	SET NOCOUNT ON;
-	-- Look for inventory credit smart entries
+	
+	-- Focus on inventory accounts whose value - when credited - is calculated using AVCO
 	WITH InventoryAccountTypes AS (
 		SELECT ATC.[Id]
 		FROM dbo.AccountTypes ATC
 		JOIN dbo.AccountTypes ATP ON ATC.[Node].IsDescendantOf(ATP.[Node])  = 1
-		WHERE ATP.[Concept] = N'Inventories' AND ATC.Concept <> N'CurrentInventoriesInTransit'
+		WHERE ATP.[Concept] = N'Inventories'
+		-- For the WIP concept, whether job or process costing, the credit value is not calculated using AVCO.
+		-- For process, we "absorb" from O/H upon declaring production
+		-- For the IIT concept, ????
+		AND ATC.Concept NOT IN (N'WorkInProgress', N'CurrentInventoriesInTransit')
 	)
+	-- Look for smart entries where one of the above inventory accounts appear on the credit side
 	INSERT INTO @AffectedLineDefinitionEntries([LineDefinitionId], [Index])
 	SELECT [LineDefinitionId], [Index]
 	FROM dbo.LineDefinitionEntries
 	WHERE [ParentAccountTypeId] IN (SELECT [Id] FROM InventoryAccountTypes)
 	AND [Direction] = -1;
-	-- Assume the debit entry comes before it (we test the assumption below)
+	-- Assume the debit entry comes before it (we test the assumption below), and that the values are balanced
 	INSERT INTO @AffectedLineDefinitionEntries([LineDefinitionId], [Index])
 	SELECT [LineDefinitionId], [Index] - 1
 	FROM @AffectedLineDefinitionEntries;
 	-- Check if posted entries are balances already
 	IF @VerifyLineDefinitions = 1
-	SELECT @BadLineDefinitionId = LD.[LineDefinitionId]
-	FROM dbo.Entries E
-	JOIN dbo.Lines L ON L.[Id] = E.[LineId]
-	JOIN dbo.Documents D ON D.[Id] = L.[DocumentId]
-	JOIN dbo.DocumentDefinitions DD ON DD.[Id] = D.[DefinitionId]
-	JOIN @AffectedLineDefinitionEntries LD ON LD.LineDefinitionId = L.[DefinitionId] AND LD.[Index] = E.[Index]
-	WHERE L.[State] = 4
-	AND DD.[DocumentType] = 2
-	GROUP BY LD.[LineDefinitionId]
-	HAVING SUM(E.[Direction] * E.[Value]) <> 0
+		SELECT @BadLineDefinitionId = LD.[LineDefinitionId]
+		FROM dbo.Entries E
+		JOIN dbo.Lines L ON L.[Id] = E.[LineId]
+		JOIN dbo.Documents D ON D.[Id] = L.[DocumentId]
+		JOIN dbo.DocumentDefinitions DD ON DD.[Id] = D.[DefinitionId]
+		JOIN @AffectedLineDefinitionEntries LD ON LD.LineDefinitionId = L.[DefinitionId] AND LD.[Index] = E.[Index]
+		WHERE L.[State] = 4
+		AND DD.[DocumentType] = 2
+		GROUP BY LD.[LineDefinitionId]
+		HAVING SUM(E.[Direction] * E.[Value]) <> 0
 	-- if not, then the assumption is wrong. Exit.
 	IF	@BadLineDefinitionId IS NOT NULL
 	BEGIN
 		DECLARE @BadLineDefinition NVARCHAR (255);
 		SELECT @BadLineDefinition = N'Improper Line Definition Design: ' + [TitleSingular] + N'. The debit should come before the credit for inventory issue.'
 		FROM dbo.LineDefinitions
+		WHERE [Id] = @BadLineDefinitionId;
 
 		RAISERROR(@BadLineDefinition, 16, 1)
 		RETURN
@@ -70,26 +77,41 @@ AS
 		SELECT ATC.[Id]
 		FROM dbo.AccountTypes ATC
 		JOIN dbo.AccountTypes ATP ON ATC.[Node].IsDescendantOf(ATP.[Node])  = 1
-		WHERE ATP.[Concept] = N'Inventories' AND ATC.Concept <> N'CurrentInventoriesInTransit'
+		WHERE ATP.[Concept] = N'Inventories'
+		-- For the WIP concept, whether job or process costing, the credit value is not calculated using AVCO.
+		-- For process, we "absorb" from O/H upon declaring production
+		-- For the IIT concept, ????
+		AND ATC.Concept NOT IN (N'WorkInProgress', N'CurrentInventoriesInTransit')
 	),
 	InventoryAccounts AS (
 		SELECT A.[Id]
 		FROM dbo.Accounts A
 		WHERE AccountTypeId IN (SELECT [Id] FROM InventoryAccountTypes)
+	),
+	AccummulatedEntries AS (
+		SELECT  E.[AccountId], E.[CustodyId], E.[ResourceId], L.PostingDate, E.[Direction], 
+			SUM(E.[Direction] * E.[BaseQuantity]) AS [AlgebraicQuantity],
+			SUM(E.[Direction] * E.[MonetaryValue]) AS [AlgebraicMonetaryValue],
+			SUM(E.[Direction] * E.[Value]) AS [AlgebraicValue]
+		FROM map.DetailsEntries() E
+		JOIN dbo.Lines L ON L.[Id] = E.[LineId]
+		JOIN dbo.Documents D ON D.[Id] = L.[DocumentId]
+		JOIN dbo.DocumentDefinitions DD ON DD.[Id] = D.[DefinitionId]
+		WHERE AccountId IN (SELECT [Id] FROM InventoryAccounts)
+		AND L.[State] = 4
+		AND DD.[DocumentType] = 2
+		GROUP BY E.[AccountId], E.[CustodyId], E.[ResourceId], L.PostingDate, E.[Direction]
 	)
-	INSERT INTO @T([PostingDate], [LineId], [Direction], [CustodyId], [ResourceId],
-				[AlgebraicQuantity], [AlgebraicMonetaryValue], [AlgebraicValue],
-				[RunningQuantity], [RunningMonetaryValue], [RunningValue])
-	SELECT L.PostingDate, L.[Id], E.[Direction], E.[CustodyId], E.[ResourceId],
-		E.[Direction] * E.[BaseQuantity], E.[Direction] * E.[MonetaryValue], E.[Direction] * E.[Value],
-			SUM(E.[Direction] * E.[BaseQuantity]) OVER (Partition BY  [ResourceId], [CustodyId] ORDER BY [PostingDate], [LineId]) AS RunningQuantity,
-			SUM(E.[Direction] * E.[MonetaryValue]) OVER (Partition BY [CustodyId], [ResourceId] ORDER BY [PostingDate], [LineId]) AS RunningMonetaryValue,
-			SUM(E.[Direction] * E.[Value]) OVER (Partition BY [CustodyId], [ResourceId] ORDER BY [PostingDate], [LineId]) AS RunningValue
-	FROM map.DetailsEntries() E
-	JOIN dbo.Lines L ON L.[Id] = E.[LineId]
-	WHERE AccountId IN (SELECT [Id] FROM InventoryAccounts)
-	AND L.[State] = 4
-	ORDER BY L.PostingDate, L.Id, Direction Desc;
+	INSERT INTO @T([AccountId], [CustodyId], [ResourceId], [PostingDate], [Direction], 
+		[AlgebraicQuantity], [AlgebraicMonetaryValue], [AlgebraicValue],
+		[RunningQuantity], [RunningMonetaryValue], [RunningValue])
+	SELECT [AccountId], [CustodyId], [ResourceId], [PostingDate], [Direction],
+		[AlgebraicQuantity], [AlgebraicMonetaryValue], [AlgebraicValue],
+		SUM([AlgebraicQuantity]) OVER (PARTITION BY [AccountId], [CustodyId], [ResourceId] ORDER BY [PostingDate], [Direction] DESC) AS RunningQuantity,
+		SUM([AlgebraicMonetaryValue]) OVER (PARTITION BY [AccountId], [CustodyId], [ResourceId] ORDER BY [PostingDate], [Direction] DESC) AS RunningMonetaryValue,
+		SUM([AlgebraicValue]) OVER (PARTITION BY [AccountId], [CustodyId], [ResourceId] ORDER BY [PostingDate], [Direction] DESC) AS RunningValue
+	FROM AccummulatedEntries
+	ORDER BY [AccountId], [CustodyId], [ResourceId], [PostingDate], [Direction] DESC;
 	
 	DECLARE @RowCount INT = -1, @PrevRowCount INT = -1, @LoopCounter INT = 0;
 	WHILE (1 = 1)
@@ -97,29 +119,34 @@ AS
 		SET @PrevRowCount = @RowCount; SET @LoopCounter = @LoopCounter + 1;
 		UPDATE @T
 		SET
-			PriorMVPU = IIF([RunningQuantity]=[AlgebraicQuantity],0,([RunningMonetaryValue] - [AlgebraicMonetaryValue]) / ([RunningQuantity] - [AlgebraicQuantity])),
-			PriorVPU =  IIF([RunningQuantity]=[AlgebraicQuantity],0,([RunningValue] - [AlgebraicValue]) /  ([RunningQuantity] - [AlgebraicQuantity]));
-		-- Look for first smart issue where the CPU has deviated from Prior CPU
+			PriorMVPU = IIF(
+				[RunningQuantity] = [AlgebraicQuantity],
+				0,
+				([RunningMonetaryValue] - [AlgebraicMonetaryValue]) / ([RunningQuantity] - [AlgebraicQuantity])),
+			PriorVPU =  IIF(
+				[RunningQuantity] = [AlgebraicQuantity],
+				0,
+				([RunningValue] - [AlgebraicValue]) / ([RunningQuantity] - [AlgebraicQuantity]));
+		-- Look for earliest smart issue dates for each item in each inventory
 		WITH BatchStartAndVPU AS (
-			SELECT MIN([Id]) As Id, [CustodyId], [ResourceId]
+			SELECT T.[AccountId], T.[CustodyId], T.[ResourceId], MIN(T.[PostingDate]) As [PostingDate]
 			FROM @T T
 			WHERE [Direction] = -1
-			AND [LineId] NOT IN (SELECT [Id] FROM dbo.Lines WHERE DefinitionId = @ManualLine)
 			AND (T.[AlgebraicMonetaryValue] / T.[AlgebraicQuantity] <> T.[PriorMVPU]
 				OR	T.[AlgebraicValue] / T.[AlgebraicQuantity] <> T.[PriorVPU])
-			GROUP BY [CustodyId], [ResourceId]
+			GROUP BY [AccountId], [CustodyId], [ResourceId]
 		),
-		-- Look for first receipt (smart or JV) where the CPU has deviated from Prior CPU
+		-- Look for first date (smart or JV) where the CPU has deviated from Prior CPU
 		BatchEnd AS (
-			SELECT
-				MIN(T.[Id]) As Id, T.[CustodyId], T.[ResourceId]
-			FROM @T AS T
-			JOIN BatchStartAndVPU BS ON T.[CustodyId] = BS.[CustodyId] AND T.[ResourceId] = BS.[ResourceId]
+			SELECT T.[AccountId], T.[CustodyId], T.[ResourceId], MIN(T.[PostingDate]) As [PostingDate]
+			FROM @T T
+			JOIN BatchStartAndVPU BS ON T.[AccountId] = BS.[AccountId] AND T.[CustodyId] = BS.[CustodyId] AND T.[ResourceId] = BS.[ResourceId]
 			WHERE [Direction] = 1
-			AND T.[Id] > BS.[Id]
+			AND T.[AlgebraicQuantity] <> 0
+			AND T.[PostingDate] > BS.[PostingDate]
 			AND (T.[AlgebraicMonetaryValue] / T.[AlgebraicQuantity] <> T.[PriorMVPU]
 				OR	T.[AlgebraicValue] / T.[AlgebraicQuantity] <> T.[PriorVPU])
-			GROUP BY T.[CustodyId], T.[ResourceId]
+			GROUP BY T.[AccountId], T.[CustodyId], T.[ResourceId]
 		)
 		-- Update all the smart inventory issues in between with the prior CPU
 		UPDATE T
@@ -127,10 +154,9 @@ AS
 			T.[AlgebraicMonetaryValue] = T.[AlgebraicQuantity] * T.[PriorMVPU],
 			T.[AlgebraicValue] = T.[AlgebraicQuantity] * T.[PriorVPU]
 		FROM @T T
-		JOIN BatchStartAndVPU BS ON T.[CustodyId] = BS.[CustodyId] AND T.[ResourceId] = BS.[ResourceId]
-		LEFT JOIN BatchEnd BE ON T.[CustodyId] = BE.[CustodyId] AND T.[ResourceId] = BE.[ResourceId]
-		WHERE T.[Id] >= BS.[Id] AND (BE.[Id] IS NULL OR T.[Id] < BE.[Id])
-		AND T.[LineId] NOT IN (SELECT [Id] FROM dbo.Lines WHERE DefinitionId = @ManualLine)
+		JOIN BatchStartAndVPU BS ON T.[AccountId] = BS.[AccountId] AND T.[CustodyId] = BS.[CustodyId] AND T.[ResourceId] = BS.[ResourceId]
+		LEFT JOIN BatchEnd BE ON T.[AccountId] = BS.[AccountId] AND T.[CustodyId] = BE.[CustodyId] AND T.[ResourceId] = BE.[ResourceId]
+		WHERE T.[PostingDate] > BS.[PostingDate] AND (BE.[PostingDate] IS NULL OR T.[PostingDate] < BE.[PostingDate])
 		AND (T.[AlgebraicMonetaryValue] <> T.[AlgebraicQuantity] * T.[PriorMVPU] OR T.[AlgebraicValue] <> T.[AlgebraicQuantity] * T.[PriorVPU]);
 		SET @RowCount = @@ROWCOUNT;
 		-- IF no changes, exit the loop
@@ -144,8 +170,8 @@ AS
 	
 		WITH CumBalances AS (
 			SELECT [Id],
-				SUM([AlgebraicMonetaryValue]) OVER (Partition BY [CustodyId], [ResourceId] ORDER BY [PostingDate], [LineId]) AS RunningMonetaryValue,
-				SUM([AlgebraicValue]) OVER (Partition BY [CustodyId], [ResourceId] ORDER BY [PostingDate], [LineId]) AS RunningValue
+				SUM([AlgebraicMonetaryValue]) OVER (Partition BY [AccountId], [CustodyId], [ResourceId] ORDER BY [PostingDate], [Direction] DESC) AS RunningMonetaryValue,
+				SUM([AlgebraicValue]) OVER (Partition BY [AccountId], [CustodyId], [ResourceId] ORDER BY [PostingDate], [Direction] DESC) AS RunningValue
 			FROM @T
 		)
 		UPDATE T
@@ -158,10 +184,10 @@ AS
 
 UPDATE E
 SET
-	E.[MonetaryValue] = -T.[AlgebraicMonetaryValue],
-	E.[Value] = -T.[AlgebraicValue]
+	E.[MonetaryValue] = ABS(T.[AlgebraicMonetaryValue]),
+	E.[Value] = ABS(T.[AlgebraicValue])
 FROM dbo.Entries E
 JOIN dbo.Lines L ON L.[Id] = E.[LineId]
-JOIN @T T ON T.[LineId] = E.[LineId]
+JOIN @T T ON T.[AccountId] = E.AccountId AND T.[CustodyId] = E.[CustodyId] AND T.[ResourceId] = E.[ResourceId] AND T.[PostingDate] = L.[PostingDate]
 JOIN @AffectedLineDefinitionEntries LD ON LD.LineDefinitionId = L.[DefinitionId] AND LD.[Index] = E.[Index]
-WHERE (E.[MonetaryValue] <> -T.[AlgebraicMonetaryValue] OR E.[Value] <> -T.[AlgebraicValue]);
+WHERE (E.[MonetaryValue] <> ABS(T.[AlgebraicMonetaryValue]) OR E.[Value] <> ABS(T.[AlgebraicValue]));
