c7ed89acc63b3251558643437bb088225b1829ab
Mohamad Akra <mohamad.akra@tellma.com>
2021-08-12 13:33:06 +0300

    fixed Expense Capitalization procedure

--- Tellma.Database.Application/wiz/Stored Procedures/wiz.ExpensesByNature__Capitalize.sql
+++ Tellma.Database.Application/wiz/Stored Procedures/wiz.ExpensesByNature__Capitalize.sql
@@ -1,20 +1,39 @@
 ﻿CREATE PROCEDURE [wiz].[ExpensesByNature__Capitalize]
-/* 4: Import, 6: Manufacturing
+/* 4: Import, 6: Manufacturing, 74: Adama, 75: AA
 	[wiz].[ExpensesByNature__Capitalize] @BusinessUnitId = 4,
-	@BSAccountTypeId = N'CurrentInventoriesInTransit', @ToDate = N'2021-04-08'
+	@CenterType = N'CurrentInventoriesInTransitExpendituresControl', @ToDate = N'2020-08-06'
 
 	[wiz].[ExpensesByNature__Capitalize] @BusinessUnitId = 4,
-	@BSAccountTypeId = N'WorkInProgress', @ToDate =  @ToDate = N'2021-02-07'
+	@CenterType = N'WorkInProgressExpendituresControl', @ToDate =  @ToDate = N'2021-02-07'
+
+	[wiz].[ExpensesByNature__Capitalize] @BusinessUnitId = 74,
+	@CenterType = N'InvestmentPropertyUnderConstructionOrDevelopmentExpendituresControl', @ToDate = N'2020-08-06'
 */
+
 	@DocumentIndex	INT = 0,
 	@BusinessUnitId INT,
 	@BSAccountTypeConcept NVARCHAR (255), --ConstructionInProgress, InvestmentPropertyUnderConstructionOrDevelopment,WorkInProgress,CurrentInventoriesInTransit
-	@FromDate		DATE,
 	@ToDate			DATE
 AS
 	DECLARE @BusinessUnitNode HIERARCHYID = (SELECT [Node] FROM dbo.[Centers] WHERE [Id] = @BusinessUnitId);
 
 	DECLARE @BSAccountTypeId INT = (SELECT [Id] FROM dbo.AccountTypes WHERE [Concept] = @BSAccountTypeConcept);
+
+	DECLARE @EntryTypeId INT = 
+		CASE
+			WHEN @BSAccountTypeConcept = N'ConstructionInProgress' THEN
+				(SELECT [Id] FROM dbo.EntryTypes WHERE [Concept] = N'???')
+			WHEN @BSAccountTypeConcept = N'InvestmentPropertyUnderConstructionOrDevelopment' THEN
+				(SELECT [Id] FROM dbo.EntryTypes WHERE [Concept] = N'???')
+			WHEN @BSAccountTypeConcept = N'WorkInProgress' THEN
+				(SELECT [Id] FROM dbo.EntryTypes WHERE [Concept] = N'???')
+			WHEN @BSAccountTypeConcept = N'CurrentInventoriesInTransit' THEN
+				(SELECT [Id] FROM dbo.EntryTypes WHERE [Concept] = N'AdditionsFromPurchasesInventoriesExtension')
+		END;
+
+	DECLARE @OpeningBalanceEntryTypeId INT = 
+		(SELECT [Id] FROM dbo.EntryTypes WHERE [Concept] = 'OpeningBalancesInventoriesExtension');
+
 	DECLARE @LineDefinitionId INT =
 		CASE
 			WHEN @BSAccountTypeConcept = N'ConstructionInProgress' THEN
@@ -37,52 +56,42 @@ AS
 			WHERE [Node].IsDescendantOf(@ExpenseByNatureNode) = 1
 		)
 	),
-	ActiveRelations AS (
-		SELECT E.[AccountId], E.[CenterId], ISNULL(E.[RelationId], -1) AS RelationId,
-			SUM(E.[Direction] * E.[Value]) AS TotalValue
-		FROM dbo.Entries E
-		JOIN dbo.Lines L ON L.[Id] = E.[LineId]
-		JOIN dbo.Accounts A ON E.[AccountId] = A.[Id]
-		JOIN dbo.Centers C ON E.[CenterId] = C.[Id]
-		WHERE L.[State] = 4
-		AND L.PostingDate <= @ToDate
-		AND A.AccountTypeId = @BSAccountTypeId
-		AND C.[Node].IsDescendantOf(@BusinessUnitNode) = 1
-		GROUP BY E.[AccountId], E.[CenterId], E.[RelationId]
-	),
 	UnCapitalizedExpenses AS (
 		SELECT MIN(E.[Id]) AS [Id], E.[AccountId], E.[CenterId], ISNULL(E.[RelationId], -1) AS RelationId,
 				E.[ResourceId], E.[UnitId], SUM(E.[Direction] * E.[Quantity]) AS [Quantity],
 				E.[CurrencyId], SUM(E.[Direction] * E.[MonetaryValue]) AS [MonetaryValue],
 				SUM(E.[Direction] * E.[Value])  AS [Value]
-		FROM Entries E
+		FROM dbo.Entries E
 		JOIN dbo.Lines L ON L.[Id] = E.[LineId]
-		JOIN dbo.Documents D ON D.[Id] = L.[DocumentId]
-		JOIN ActiveRelations AR ON E.[CenterId] = AR.[CenterId] AND E.[RelationId] = AR.[RelationId] -- To exclude non capitalizable expenses
+		JOIN dbo.Centers C ON E.[CenterId] = C.[Id]
 		WHERE L.[State] = 4
 		AND E.[AccountId] IN (SELECT [Id] FROM ExpenseByNatureAccounts)
-		AND L.PostingDate BETWEEN @FromDate AND @ToDate
-		AND ISNULL(E.[RelationId], -1) = AR.[RelationId]
+		AND C.CenterType = @CenterType
+		AND C.[Node].IsDescendantOf(@BusinessUnitNode) = 1
+		AND L.PostingDate <= @ToDate
 		GROUP BY E.[AccountId],  E.[CenterId], E.[RelationId], E.[ResourceId], E.[UnitId], E.[CurrencyId]
 		HAVING SUM(E.[Direction] * E.[Value]) <> 0
-	),
+	),--select * from UnCapitalizedExpenses
 	TargetResources AS (
 		SELECT E.[AccountId], E.[CenterId], ISNULL(E.[RelationId], -1) AS RelationId,
-			E.[ResourceId], SUM(E.[Direction] * E.[Value]) AS NetValue
+			E.[ResourceId], SUM(E.[Direction] * E.[BaseQuantity]) AS NetQuantity, SUM(E.[Direction] * E.[Value]) AS NetValue
 		FROM map.DetailsEntries() E
 		JOIN dbo.Accounts A ON E.[AccountId] = A.[Id]
 		JOIN dbo.Lines L ON L.[Id] = E.[LineId]
-		JOIN dbo.Documents D ON D.[Id] = L.[DocumentId]
-		JOIN ActiveRelations AR ON
-			E.[AccountId] = AR.[AccountId] 
-			AND E.[CenterId] = AR.[CenterId]
-			AND ISNULL(E.[RelationId], -1) = AR.[RelationId]
+		JOIN UnCapitalizedExpenses C ON
+			E.[CenterId] = C.[CenterId]
+			AND ISNULL(E.[RelationId], -1) = C.[RelationId]
 		WHERE L.[State] = 4
-		AND L.PostingDate BETWEEN @FromDate AND @ToDate
 		AND A.[AccountTypeId] = @BSAccountTypeId
+		AND E.EntryTypeId IN (@EntryTypeId, @OpeningBalanceEntryTypeId) -- . Works for IIT
 		GROUP BY E.[AccountId], E.[CenterId], E.[RelationId], E.[ResourceId]
 		HAVING SUM(E.[Direction] * E.[Value]) <> 0
 	), -- select * from TargetResources
+	ActiveRelations AS (
+		SELECT [AccountId], [CenterId], [RelationId], SUM([NetQuantity]) AS [TotalQuantity], SUM([NetValue]) AS TotalValue
+		FROM TargetResources
+		GROUP BY [AccountId], [CenterId], [RelationId]
+	),
 	ExpenseDistribution AS (
 		SELECT U.[Id], U.[AccountId] AS [AccountId1], U.[ResourceId] AS [ResourceId1],
 				NULL AS [NotedRelationId1], U.[UnitId] AS [UnitId1],
@@ -101,7 +110,7 @@ AS
 			ON AR.[AccountId] = T.[AccountId]
 			AND U.[CenterId] = T.[CenterId] AND U.[RelationId] = T.[RelationId]
 		JOIN dbo.Resources R ON R.[Id] = T.[ResourceId]
-	)
+	) -- select * from ExpenseDistribution
 	INSERT INTO @WideLines([Index], [DefinitionId],
 			[DocumentIndex], [AccountId0], [CenterId0], [RelationId0], [ResourceId0],[Quantity0],[UnitId0],
 			[AccountId1], [CenterId1], [RelationId1], [ResourceId1], [NotedRelationId1], [CurrencyId1],
