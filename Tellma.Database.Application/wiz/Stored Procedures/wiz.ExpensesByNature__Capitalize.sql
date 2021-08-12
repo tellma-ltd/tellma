bad2a5d4931882ab16cbbe19d3d98ceb7526bf2d
Mohamad Akra <mohamad.akra@tellma.com>
2021-08-12 13:34:25 +0300

    Fixed Expense capitalization procedure

--- Tellma.Database.Application/wiz/Stored Procedures/wiz.ExpensesByNature__Capitalize.sql
+++ Tellma.Database.Application/wiz/Stored Procedures/wiz.ExpensesByNature__Capitalize.sql
@@ -1,7 +1,7 @@
 ﻿CREATE PROCEDURE [wiz].[ExpensesByNature__Capitalize]
 /* 4: Import, 6: Manufacturing, 74: Adama, 75: AA
 	[wiz].[ExpensesByNature__Capitalize] @BusinessUnitId = 4,
-	@CenterType = N'CurrentInventoriesInTransitExpendituresControl', @ToDate = N'2021-03-09'
+	@CenterType = N'CurrentInventoriesInTransitExpendituresControl', @ToDate = N'2020-08-06'
 
 	[wiz].[ExpensesByNature__Capitalize] @BusinessUnitId = 4,
 	@CenterType = N'WorkInProgressExpendituresControl', @ToDate =  @ToDate = N'2021-02-07'
@@ -15,7 +15,6 @@
 	@ToDate			DATE
 WITH RECOMPILE
 AS
-	DECLARE @FromDate DATE = (SELECT [ArchiveDate] FROM dbo.[Settings]);
 	DECLARE @BusinessUnitNode HIERARCHYID = (SELECT [Node] FROM dbo.[Centers] WHERE [Id] = @BusinessUnitId);
 	DECLARE @BSAccountTypeId INT =
 		CASE
@@ -28,6 +27,20 @@ AS
 			WHEN @CenterType = N'CurrentInventoriesInTransitExpendituresControl' THEN
 				(SELECT [Id] FROM dbo.AccountTypes WHERE [Concept] = N'CurrentInventoriesInTransit')
 		END;
+
+	DECLARE @EntryTypeId INT = 
+		CASE
+			WHEN @CenterType = N'ConstructionInProgressExpendituresControl' THEN
+				(SELECT [Id] FROM dbo.EntryTypes WHERE [Concept] = N'???')
+			WHEN @CenterType = N'InvestmentPropertyUnderConstructionOrDevelopmentExpendituresControl' THEN
+				(SELECT [Id] FROM dbo.EntryTypes WHERE [Concept] = N'???')
+			WHEN @CenterType = N'WorkInProgressExpendituresControl' THEN
+				(SELECT [Id] FROM dbo.EntryTypes WHERE [Concept] = N'???')
+			WHEN @CenterType = N'CurrentInventoriesInTransitExpendituresControl' THEN
+				(SELECT [Id] FROM dbo.EntryTypes WHERE [Concept] = N'AdditionsFromPurchasesInventoriesExtension')
+		END;
+	DECLARE @OpeningBalanceEntryTypeId INT = 
+			(SELECT [Id] FROM dbo.EntryTypes WHERE [Concept] = 'OpeningBalancesInventoriesExtension');
 	DECLARE @LineDefinitionId INT =
 		CASE
 			WHEN @CenterType = N'ConstructionInProgressExpendituresControl' THEN
@@ -49,60 +62,49 @@ AS
 			WHERE [CenterType] = N'Expenditure'
 		)
 	),
-	ActiveCustodies AS (
-		SELECT E.[AccountId], E.[CenterId], ISNULL(E.[CustodyId], -1) AS CustodyId,
-			SUM(E.[Direction] * E.[Value]) AS TotalValue
-		FROM dbo.Entries E
-		JOIN dbo.Lines L ON L.[Id] = E.[LineId]
-		JOIN dbo.Accounts A ON E.[AccountId] = A.[Id]
-		JOIN dbo.Centers C ON E.[CenterId] = C.[Id]
-		WHERE L.[State] = 4
-		AND L.PostingDate <= @ToDate
-		AND A.AccountTypeId = @BSAccountTypeId
-		AND C.[Node].IsDescendantOf(@BusinessUnitNode) = 1
-		GROUP BY E.[AccountId], E.[CenterId], E.[CustodyId]
-		HAVING SUM(E.[Direction] * E.[Value]) <> 0
-	),
 	UnCapitalizedExpenses AS (
 		SELECT MIN(E.[Id]) AS [Id], E.[AccountId], E.[CenterId], ISNULL(E.[CustodyId], -1) AS [CustodyId], 
 			E.[ResourceId], E.[UnitId], SUM(E.[Direction] * E.[Quantity]) AS [Quantity],
 			E.[CurrencyId], SUM(E.[Direction] * E.[MonetaryValue]) AS [MonetaryValue],
 			SUM(E.[Direction] * E.[Value])  AS [Value]
-		FROM Entries E
+		FROM dbo.Entries E
 		JOIN dbo.Lines L ON L.[Id] = E.[LineId]
-		JOIN dbo.Documents D ON D.[Id] = L.[DocumentId]
-		JOIN dbo.DocumentDefinitions DD ON DD.[Id] = D.[DefinitionId]
-		JOIN ActiveCustodies C ON E.[CenterId] = C.[CenterId] 	
-		WHERE DD.DocumentType = 2 -- event
-		AND L.[State] = 4
+		JOIN dbo.Centers C ON E.[CenterId] = C.[Id]
+		WHERE L.[State] = 4
 		AND E.[AccountId] IN (SELECT [Id] FROM ExpenseByNatureAccounts)
-		AND L.PostingDate BETWEEN @FromDate AND @ToDate
-		AND ISNULL(E.[CustodyId], -1) = C.[CustodyId]
+		AND C.CenterType = @CenterType
+		AND C.[Node].IsDescendantOf(@BusinessUnitNode) = 1
+		AND L.PostingDate <= @ToDate
 		GROUP BY E.[AccountId],  E.[CenterId], E.[CustodyId], E.[ResourceId], E.[UnitId], E.[CurrencyId]
 		HAVING SUM(E.[Direction] * E.[Value]) <> 0
-	),
-	--select * from UnCapitalizedExpenses
+	),	--select * from UnCapitalizedExpenses
 	TargetResources AS (
-		SELECT E.[AccountId], E.[CenterId], ISNULL(E.[CustodyId], -1) AS CustodyId, E.[ResourceId], SUM(E.[Direction] * E.[Value]) AS NetValue
+		-- all resources shipped back and future on the capitalized expenses shipments
+		SELECT E.[AccountId], E.[CenterId], ISNULL(E.[CustodyId], -1) AS CustodyId, E.[ResourceId],
+			SUM(E.[Direction] * E.[BaseQuantity]) AS NetQuantity,
+			SUM(E.[Direction] * E.[Value]) AS NetValue
 		FROM map.DetailsEntries() E
 		JOIN dbo.Accounts A ON E.[AccountId] = A.[Id]
 		JOIN dbo.Lines L ON L.[Id] = E.[LineId]
-		JOIN dbo.Documents D ON D.[Id] = L.[DocumentId]
-		JOIN ActiveCustodies C ON
-			E.[AccountId] = C.[AccountId] 
-			AND E.[CenterId] = C.[CenterId]
+		JOIN UnCapitalizedExpenses C ON
+			E.[CenterId] = C.[CenterId]
 			AND  ISNULL(E.[CustodyId], -1) = C.[CustodyId]
 		WHERE L.[State] = 4
-		AND L.PostingDate BETWEEN @FromDate AND @ToDate
 		AND A.[AccountTypeId] = @BSAccountTypeId
+		AND E.EntryTypeId IN (@EntryTypeId, @OpeningBalanceEntryTypeId) -- . Works for IIT
 		GROUP BY E.[AccountId], E.[CenterId], E.[CustodyId], E.[ResourceId]
 		HAVING SUM(E.[Direction] * E.[Value]) <> 0
+	), --select * from TargetResources;
+	ActiveCustodies AS (
+		SELECT  [AccountId], [CenterId], [CustodyId], SUM([NetQuantity]) AS [TotalQuantity],
+				SUM([NetValue]) AS [TotalValue]
+		FROM TargetResources
+		GROUP BY [AccountId], [CenterId], [CustodyId]
 	),
-	--select * from TargetResources
 	ExpenseDistribution AS (
 		SELECT U.[Id], U.[AccountId] AS [AccountId1], U.[ResourceId] AS [ResourceId1],
 				NULL AS [ParticipantId1], U.[UnitId] AS [UnitId1],
-				U.[Quantity] * T.[NetValue] / C.[TotalValue] AS [Quantiy1],
+				U.[Quantity] * T.[NetValue] / C.[TotalValue] AS [Quantity1],
 				U.[CurrencyId] AS [Currencyid1],
 				U.[MonetaryValue] * T.[NetValue] / C.[TotalValue] AS [MonetaryValue1],
 				U.[Value] * T.[NetValue] / C.[TotalValue] AS [Value1],
@@ -118,8 +120,7 @@ AS
 			AND U.[CenterId] = T.[CenterId]
 			AND U.[CustodyId] = T.[CustodyId]
 		JOIN dbo.Resources R ON R.[Id] = T.[ResourceId]
-	)
---	select * from ExpenseDistribution
+	) -- select * from ExpenseDistribution
 
 	INSERT INTO @WideLines([Index], [DefinitionId],
 			[DocumentIndex],[AccountId0], [CenterId0], [CustodyId0], [ResourceId0],[Quantity0],[UnitId0],
