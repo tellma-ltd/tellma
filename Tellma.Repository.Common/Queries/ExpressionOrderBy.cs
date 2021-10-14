c3a25bf1f89e65ae9b0dd9a9821d3cc3d56923fa
Ahmad Akra <ahmad.akra@tellma.com>
2021-10-14 11:12:37 +0300

    Fixed bug when ordering data in details reports and drillthrough

--- Tellma.Repository.Common/Queries/ExpressionOrderBy.cs
+++ Tellma.Repository.Common/Queries/ExpressionOrderBy.cs
@@ -66,11 +66,11 @@ namespace Tellma.Repository.Common
             }
 
             var expressions = QueryexBase.Parse(orderby, expectDirKeywords: true);
-            var nonColumnAccess = expressions.FirstOrDefault(e => !(e is QueryexColumnAccess));
-            if (nonColumnAccess != null)
-            {
-                throw new QueryException($"Select parameter cannot contain an expression like {nonColumnAccess}, only column access expressions are allowed.");
-            }
+            //var nonColumnAccess = expressions.FirstOrDefault(e => !(e is QueryexColumnAccess));
+            //if (nonColumnAccess != null)
+            //{
+            //    throw new QueryException($"OrderBy parameter cannot contain an expression like {nonColumnAccess}, only column access expressions are allowed.");
+            //}
 
             return new ExpressionOrderBy(expressions);
         }
