1b932c242038ec1249349df613cded5c1cf5fc6c
Ahmad Akra <ahmad.akra@tellma.com>
2021-10-14 11:12:54 +0300

    Removed commented code

--- Tellma.Repository.Common/Queries/ExpressionOrderBy.cs
+++ Tellma.Repository.Common/Queries/ExpressionOrderBy.cs
@@ -66,12 +66,6 @@ namespace Tellma.Repository.Common
             }
 
             var expressions = QueryexBase.Parse(orderby, expectDirKeywords: true);
-            //var nonColumnAccess = expressions.FirstOrDefault(e => !(e is QueryexColumnAccess));
-            //if (nonColumnAccess != null)
-            //{
-            //    throw new QueryException($"OrderBy parameter cannot contain an expression like {nonColumnAccess}, only column access expressions are allowed.");
-            //}
-
             return new ExpressionOrderBy(expressions);
         }
     }
