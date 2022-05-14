1071cbb108da08583520c084303d942d9c6bb101
Mohamad Akra <mohamad.akra@tellma.com>
2022-05-14 21:56:03 +0300

    Added SignScript to Lines Validate Sign

--- Tellma.Database.Application/bll/Stored Procedures/bll.Lines_Validate__Sign.sql
+++ Tellma.Database.Application/bll/Stored Procedures/bll.Lines_Validate__Sign.sql
@@ -227,4 +227,52 @@ BEGIN
 		@Entries = @Entries, 
 		@State = @ToState,
 		@IsError = @IsError OUTPUT;
+
+	DECLARE @PreScript NVARCHAR(MAX) = N'
+	SET NOCOUNT ON
+	DECLARE @ValidationErrors [dbo].[ValidationErrorList];
+	------
+	';
+	DECLARE @Script NVARCHAR (MAX);
+	DECLARE @PostScript NVARCHAR(MAX) = N'
+	-----
+	SELECT TOP (@Top) * FROM @ValidationErrors;
+	';
+
+	DECLARE @SignValidateScriptLineDefinitions [dbo].[StringList], @LineDefinitionId INT;
+	DECLARE @LineState SMALLINT, @D DocumentList, @L LineList, @E EntryList;
+	INSERT INTO @SignValidateScriptLineDefinitions
+	SELECT DISTINCT DefinitionId FROM @Lines
+	WHERE DefinitionId IN (
+		SELECT [Id] FROM dbo.LineDefinitions
+		WHERE [SignValidateScript] IS NOT NULL
+	);
+
+	IF EXISTS (SELECT * FROM @SignValidateScriptLineDefinitions)
+	BEGIN
+		-- run script to validate information
+		DECLARE LineDefinition_Cursor CURSOR FOR SELECT [Id] FROM @SignValidateScriptLineDefinitions;  
+		OPEN LineDefinition_Cursor  
+		FETCH NEXT FROM LineDefinition_Cursor INTO @LineDefinitionId; 
+		WHILE @@FETCH_STATUS = 0  
+		BEGIN 
+			SELECT @Script =  @PreScript + ISNULL([SignValidateScript],N'') + @PostScript
+			FROM dbo.LineDefinitions WHERE [Id] = @LineDefinitionId;
+			DELETE FROM @L; DELETE FROM @E;
+			INSERT INTO @L SELECT * FROM @Lines WHERE DefinitionId = @LineDefinitionId
+			INSERT INTO @E SELECT E.* FROM @Entries E JOIN @L L ON E.LineIndex = L.[Index] AND E.DocumentIndex = L.DocumentIndex
+			INSERT INTO @ValidationErrors
+			EXECUTE	dbo.sp_executesql @Script, N'
+				@LineDefinitionId INT,
+				@ToState SMALLINT,
+				@Documents [dbo].[DocumentList] READONLY,
+				@DocumentLineDefinitionEntries [dbo].[DocumentLineDefinitionEntryList] READONLY,
+				@Lines [dbo].[LineList] READONLY, 
+				@Entries [dbo].EntryList READONLY,
+				@Top INT', 	@LineDefinitionId = @LineDefinitionId, @ToState = @ToState, @Documents = @Documents,
+				@DocumentLineDefinitionEntries = @DocumentLineDefinitionEntries, @Lines = @L, @Entries = @E, @Top = @Top;
+			
+			FETCH NEXT FROM LineDefinition_Cursor INTO @LineDefinitionId;
+		END
+	END
 END;
