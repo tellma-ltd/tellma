bd0d63d909fbd41d8e6ee4293bf3d2add58762ae
Mohamad Akra <mohamad.akra@tellma.com>
2022-08-18 18:06:57 +0300

    fixed a bug in Date period conversion function

--- Tellma.Database.Application/dbo/Functions/dbo.fn_FromDate_ToDate__WorkdaysText.sql
+++ Tellma.Database.Application/dbo/Functions/dbo.fn_FromDate_ToDate__WorkdaysText.sql
@@ -8,11 +8,30 @@ AS
 BEGIN
 	DECLARE @Years INT, @Months INT, @Days INT, @YDate DATE, @MDate DATE;
 	SET @ToDate = DATEADD(DAY, 1, @ToDate);
-	SET @Years = DATEDIFF(YEAR, @FromDate, @ToDate); --print N'Years = ' + cast(@years as nvarchar(50))
+	SET @Years =
+		CASE 
+			WHEN
+				DATEPART(YEAR, @FromDate) < DATEPART(YEAR, @ToDate)
+			AND (DATEPART(MONTH, @FromDate) < DATEPART(MONTH, @ToDate)
+				OR (DATEPART(MONTH, @FromDate) = DATEPART(MONTH, @ToDate) 
+					AND	DATEPART(DAY, @FromDate) <= DATEPART(DAY, @ToDate))
+				)
+				THEN DATEDIFF(YEAR, @FromDate, @ToDate)
+            WHEN DATEPART(YEAR, @FromDate) < DATEPART(YEAR, @ToDate)
+				THEN DATEDIFF(YEAR, @FromDate, @ToDate) - 1
+            WHEN DATEPART(YEAR, @FromDate) = DATEPART(YEAR, @ToDate)
+				THEN 0
+        END; --print N'Years = ' + cast(@years as nvarchar(50))
 	SET @FromDate = DATEADD(YEAR, @Years, @FromDate); --print @fromDate
-	SET @Months = DATEDIFF(MONTH, @FromDate, @ToDate); --print N'Months = ' + cast(@Months as nvarchar(50))
+	SET @Months =
+		CASE 
+            WHEN DATEPART(DAY, @FromDate) > DATEPART(DAY, @ToDate)
+				THEN DATEDIFF(MONTH, @FromDate, @ToDate) - 1
+            ELSE DATEDIFF(MONTH, @FromDate, @ToDate)
+		END; --print N'Months = ' + cast(@Months as nvarchar(50))
 	SET @FromDate = DATEADD(MONTH, @Months, @FromDate); --print @fromDate
 	SET @Days = DATEDIFF(DAY, @FromDate, @ToDate); --print N'Days = ' + cast(@Days as nvarchar(50))
 	
-	RETURN CAST(@Years AS NVARCHAR (50)) + N'Y-' + CAST(@Months AS NVARCHAR (50))  + N'M--' + CAST(@Days AS NVARCHAR (50)) + N'D'
+	RETURN CAST(@Years AS NVARCHAR (50)) + N'Y-' + CAST(@Months AS NVARCHAR (50)) + N'M-' + CAST(@Days AS NVARCHAR (50)) + N'D'
 END
+GO
