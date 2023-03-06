b7cfc57d476f6806e615672d377482d84293196b
Mohamad Akra <m_akra@msn.com>
2023-03-06 06:11:52 +0300

    Added Trade specific functions

---
+++ Tellma.Database.Application/dal/Functions/dal.fn_Concept_Center_Currency_TradeAgent__Balance.sql
@@ -0,0 +1,20 @@
+﻿CREATE FUNCTION [dal].[fn_Concept_Center_Currency_TradeAgent__Balance]
+(
+	@ParentConcept NVARCHAR (255),
+	@ParentCenterId INT,
+	@CurrencyId NCHAR (3),
+	@AgentId INT,
+	@NotedDate DATE
+)
+RETURNS DECIMAL (19,4)
+AS BEGIN
+DECLARE @Result  DECIMAL (19,4), @ParentNode HIERARCHYID = dal.fn_AccountTypeConcept__Node(@ParentConcept);
+	SELECT @Result = SUM([Balance])
+	FROM [dal].[ft_Concept_Center__TradeAgents_Balances](@ParentConcept, @ParentCenterId) T
+	JOIN dbo.Agents AG ON AG.[Id] = T.[AgentId]
+	WHERE T.[CurrencyId] = @CurrencyId
+	AND T.[AgentId] = @AgentId
+	AND (@NotedDate IS NULL			AND [NotedDate] IS NULL			OR AG.[ToDate] <= @NotedDate)
+	RETURN ISNULL(@Result, 0)
+END
+GO
--- Tellma.Database.Application/wiz/Stored Procedures/wiz.ToCashFromTradeReceivable.sql
+++ Tellma.Database.Application/wiz/Stored Procedures/wiz.ToCashFromTradeReceivable.sql
@@ -23,14 +23,14 @@ AS
 		[AccountId1], [CenterId1], [AgentId1], [MonetaryValue1], [NotedAmount1], [CurrencyId1], [NotedDate1],
 		[MonetaryValue0], [CurrencyId0], [Value1])
 	SELECT ROW_NUMBER() OVER(ORDER BY SI.[Id]) - 1, 0,
-		SS.[AccountId], SS.[CenterId], SS.[AgentId], SUM(SS.[Balance]), SUM(SS.[Balance]), SS.[CurrencyId], MAX(SI.[ToDate]) AS [NotedDate1],
+		SS.[AccountId], SS.[CenterId], SS.[AgentId], SUM(SS.[Balance]), SUM(SS.[Balance]), SS.[CurrencyId], SI.[ToDate] AS [NotedDate1],
 		bll.fn_ConvertCurrencies(@PostingDate, SS.[CurrencyId], @CurrencyId0, SUM(SS.[Balance])) AS [MonetaryValue0], @CurrencyId0,
 		bll.fn_ConvertToFunctional(@PostingDate, SS.[CurrencyId], SUM(SS.[Balance]))
 	FROM [dal].[ft_Concept_Center__Agents_Balances](N'CurrentTradeReceivables', NULL) SS
 	JOIN dbo.Agents SI ON SI.[Id] = SS.[AgentId]
 	WHERE SI.[Agent1Id] = @TradeReceivableAccountId
 	AND ([SI].[ToDate] IS NULL OR [SI].[ToDate] <= ISNULL(@DueOnOrBefore, @PostingDate))
-	GROUP BY SI.[Id], SS.[AccountId], SS.[CenterId], SS.[AgentId], SS.[CurrencyId]
+	GROUP BY SI.[Id], SS.[AccountId], SS.[CenterId], SS.[AgentId], SS.[CurrencyId], SI.[ToDate]
 	HAVING SUM(SS.[Balance]) > 0
 
 	SELECT * FROM @WideLines;
