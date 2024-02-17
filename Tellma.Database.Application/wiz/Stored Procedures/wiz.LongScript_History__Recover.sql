f7f04d3ee1f86b14a9ddc043fbeefeadd297e65e
Mohamad Akra <m_akra@msn.com>
2024-02-17 19:24:50 +0300

    Added useful script

---
+++ Tellma.Database.Application/wiz/Stored Procedures/wiz.LongScript_History__Recover.sql
@@ -0,0 +1,15 @@
+﻿CREATE PROCEDURE [wiz].[LongScript_History__Recover]
+AS
+-- This is a sample code.
+-- Please replace table and script name
+-- @DefinitionId is the Id of LineDefinition, AgentDefinition or ResourceDefinition
+-- If you want to restore the last script, @ValidFrom is typically the last one in the history table
+DECLARE @DefinitionId INT = 262;
+DECLARE @ValidFrom DATETIME2 = '2023-12-03 14:35:00.7483217';
+DECLARE @S NVARCHAR (512) = N'NULL', @I INT = 0;
+WHILE LEN(@S) > 0
+BEGIN
+	SELECT @S = SUBSTRING(ValidateScript, @I, 512), @I = @I + 512
+	FROM LineDefinitionsHistory where Id = @DefinitionId and ValidFrom = @ValidFrom
+	PRINT @S
+END
