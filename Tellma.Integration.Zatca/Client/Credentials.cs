7faf10b816d5ded829c4773b31b7c37f1efbedb9
mohamadakra <mohamad.akra@tellma.co>
2024-02-18 23:36:04 -0800

    Refactor a bit

---
+++ Tellma.Integration.Zatca/Client/Credentials.cs
@@ -0,0 +1,24 @@
+﻿namespace Tellma.Integration.Zatca
+{
+    public class Credentials
+    {
+        public Credentials(string username, string password)
+        {
+            if (string.IsNullOrWhiteSpace(username))
+            {
+                throw new ArgumentException($"'{nameof(username)}' cannot be null or whitespace.", nameof(username));
+            }
+
+            if (string.IsNullOrWhiteSpace(password))
+            {
+                throw new ArgumentException($"'{nameof(password)}' cannot be null or whitespace.", nameof(password));
+            }
+
+            Username = username;
+            Password = password;
+        }
+
+        public string Username { get; }
+        public string Password { get; }
+    }
+}
--- Tellma.Integration.Zatca/Client/ZatcaClient.cs
+++ Tellma.Integration.Zatca/Client/ZatcaClient.cs
@@ -1,32 +1,10 @@
 ﻿using System.Net;
-using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Net.Http.Json;
 using System.Text.Json;
 
 namespace Tellma.Integration.Zatca
 {
-    public class Credentials
-    {
-        public Credentials(string username, string password)
-        {
-            if (string.IsNullOrWhiteSpace(username))
-            {
-                throw new ArgumentException($"'{nameof(username)}' cannot be null or whitespace.", nameof(username));
-            }
-
-            if (string.IsNullOrWhiteSpace(password))
-            {
-                throw new ArgumentException($"'{nameof(password)}' cannot be null or whitespace.", nameof(password));
-            }
-
-            Username = username;
-            Password = password;
-        }
-
-        public string Username { get; }
-        public string Password { get; }
-    }
     public class ZatcaClient
     {
         #region Constants
