4179ea107626c4542dbf7cd48172756da2ea4668
Mohamad Akra <m_akra@msn.com>
2024-02-26 00:21:26 +0300

    Zatca update

--- Tellma.Database.Application/dal/Stored Procedures/dal.Zatca__GetInvoices.sql
+++ Tellma.Database.Application/dal/Stored Procedures/dal.Zatca__GetInvoices.sql
@@ -1,4 +1,4 @@
-﻿CREATE PROCEDURE [dal].[Zatca__GetInvoices] -- declare @Ids indexedidlist, @PIS INT, @PIH nvarchar(max);insert into @Ids values (0, 17038); exec [dal].[Zatca__GetInvoices] @Ids, @PIS, @PIH
+﻿CREATE [dal].[Zatca__GetInvoices] -- declare @Ids indexedidlist, @PIS INT, @PIH nvarchar(max);insert into @Ids values (0, 17038); exec [dal].[Zatca__GetInvoices] @Ids, @PIS, @PIH
 	@Ids [dbo].[IndexedIdList] READONLY,
     @PreviousInvoiceSerialNumber INT OUTPUT,
     @PreviousInvoiceHash NVARCHAR(MAX) OUTPUT
@@ -23,7 +23,7 @@ BEGIN
 		INNER JOIN dbo.AccountTypes AC ON AC.[Id] = A.[AccountTypeId]
 		INNER JOIN @Ids AS I ON I.[Id] = L.[DocumentId]
 		INNER JOIN dbo.Documents D ON D.[Id] = I.[Id]
-		INNER JOIN dbo.DocumentDefinitions DD ON DD.[Id] = D.[DocumentId]
+		INNER JOIN dbo.DocumentDefinitions DD ON DD.[Id] = D.[DefinitionId]
 		WHERE AC.[Concept] = N'CurrentValueAddedTaxPayables'
 		AND DD.[ZatcaDocumentType] IS NOT NULL
 	) RETURN
@@ -44,20 +44,7 @@ BEGIN
 			FROM dbo.Documents
 			WHERE [ZatcaSerialNumber] = @PreviousInvoiceSerialNumber
 		);
-    
-	--=-=-= 0 - Exit clause =-=-=--
-	IF NOT EXISTS (
-		SELECT * FROM [map].[Lines]() L
-		INNER JOIN dbo.Entries E ON E.[LineId] = L.[Id]
-		INNER JOIN dbo.Accounts A ON A.[Id] = E.[AccountId]
-		INNER JOIN dbo.AccountTypes AC ON AC.[Id] = A.[AccountTypeId]
-		INNER JOIN @Ids AS I ON I.[Id] = L.[DocumentId]
-		INNER JOIN dbo.Documents D ON D.[Id] = I.[Id]
-		INNER JOIN dbo.DocumentDefinitions DD ON DD.[Id] = D.[DocumentId]
-		WHERE AC.[Concept] = N'CurrentValueAddedTaxPayables'
-		AND DD.[ZatcaDocumentType]
-	) RETURN
-
+  
     --=-=-= 1 - Invoices =-=-=--
 	SELECT
 		I.[Index] AS [Index],
