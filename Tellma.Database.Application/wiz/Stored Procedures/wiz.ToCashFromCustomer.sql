9683d6815918c31af0d0158c5f7cc2dc077a23a6
Mohamad Akra <m_akra@msn.com>
2026-08-25 13:34:15 +0200

    Bug fixes

--- Tellma.Database.Application/wiz/Stored Procedures/wiz.ToCashFromCustomer.sql
+++ Tellma.Database.Application/wiz/Stored Procedures/wiz.ToCashFromCustomer.sql
@@ -3,7 +3,8 @@
 	@DueOnOrBefore DATE,
 	@CashAccountId INT,
 	@PostingDate DATE = NULL,
-	@DueOnOrAfter DATE = NULL
+	@DueOnOrAfter DATE = NULL,
+	@ReceivedAmount DECIMAL (19, 6) = NULL
 AS
 	DECLARE @CurrencyId0 NCHAR (3) = dal.fn_Agent__CurrencyId(@CashAccountId);
 	SET @PostingDate = ISNULL(@PostingDate, GETDATE());
@@ -17,7 +18,7 @@ AS
 	INSERT INTO @WideLines([Index], [DocumentIndex],
 		[AccountId1], [CenterId1], [AgentId1], [MonetaryValue1], [NotedAmount1], [CurrencyId1], [NotedDate1],
 		[MonetaryValue0], [CurrencyId0], [Value1])
-	SELECT ROW_NUMBER() OVER(ORDER BY SI.[Id]) - 1, 0,
+	SELECT ROW_NUMBER() OVER(ORDER BY SI.[ToDate], SI.[Id]) - 1, 0,
 		SS.[AccountId], SS.[CenterId], SS.[AgentId], SUM(SS.[Balance]), SUM(SS.[Balance]), SS.[CurrencyId], SI.[ToDate] AS [NotedDate1],
 		bll.fn_ConvertCurrencies(@PostingDate, SS.[CurrencyId], @CurrencyId0, SUM(SS.[Balance])) AS [MonetaryValue0], @CurrencyId0,
 		bll.fn_ConvertToFunctional(@PostingDate, SS.[CurrencyId], SUM(SS.[Balance]))
@@ -28,7 +29,49 @@ AS
 	AND ([SI].[ToDate] IS NULL OR [SI].[ToDate] <= ISNULL(@DueOnOrBefore, @PostingDate))
 	AND ([SI].[ToDate] IS NULL OR [SI].[ToDate] >= ISNULL(@DueOnOrAfter, N'1753-01-01'))
 	GROUP BY SI.[Id], SS.[AccountId], SS.[CenterId], SS.[AgentId], SS.[CurrencyId], SI.[ToDate]
-	HAVING SUM(SS.[Balance]) > 0
+	HAVING SUM(SS.[Balance]) > 0;
+
+	-- The above may return multiple lines
+	-- MonetaryValue0 = Received Amount in Cash account currency for the given sales invoice
+	-- MonetaryValue1 = Equivalent to Received Amount but in customer account currency for the given sales invoice
+	-- NotedAmount1 = Due Amount in Customer account currency for the given sales invoice
+	-- SI.[ToDate] = Due date of the sales invoice
+	-- SI.[Agent1Id] = Customer account
+IF @ReceivedAmount IS NOT NULL
+	BEGIN
+		DECLARE @LastIndex INT;
+		DECLARE @Excess DECIMAL(19, 6);
+
+		-- Calculate running total to locate @LastIndex and the excess over @ReceivedAmount
+		WITH RunningTotals AS (
+			SELECT 
+				[Index],
+				[MonetaryValue0],
+				SUM([MonetaryValue0]) OVER (ORDER BY [Index]) AS [CumulativeTotal]
+			FROM @WideLines
+		)
+		SELECT TOP 1 
+			@LastIndex = [Index],
+			@Excess = [CumulativeTotal] - @ReceivedAmount
+		FROM RunningTotals
+		WHERE [CumulativeTotal] >= @ReceivedAmount
+		ORDER BY [Index];
+
+		IF @LastIndex IS NOT NULL
+		BEGIN
+			-- DELETE @Widelines WHERE [Index] > @LastIndex
+			DELETE FROM @WideLines 
+			WHERE [Index] > @LastIndex;
+
+			-- UPDATE @Widelines[@LastIndex] SO that SUM([MonetaryValue0]) = @ReceivedAmount
+			UPDATE @WideLines
+			SET 
+				[MonetaryValue1] = CASE WHEN [MonetaryValue0] <> 0 THEN [MonetaryValue1] * ([MonetaryValue0] - @Excess) / [MonetaryValue0] ELSE 0 END,
+				[Value1]         = CASE WHEN [MonetaryValue0] <> 0 THEN [Value1] * ([MonetaryValue0] - @Excess) / [MonetaryValue0] ELSE 0 END,
+				[MonetaryValue0] = [MonetaryValue0] - @Excess
+			WHERE [Index] = @LastIndex;
+		END
+	END
 
 	SELECT * FROM @WideLines;
 GO
