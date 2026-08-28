ff0cdb6568c1ef45706a317abe117f983f91f627
Ahmad Akra <ahmad.akra@tellma.com>
2026-08-28 06:45:59 -0700

    Revert the 50-255 char change

--- Tellma.Model.Application/Entry.cs
+++ Tellma.Model.Application/Entry.cs
@@ -89,7 +89,7 @@ namespace Tellma.Model.Application
         public string InternalReference { get; set; }
 
         [Display(Name = "Entry_NotedAgentName")]
-        [StringLength(255)]
+        [StringLength(50)]
         public string NotedAgentName { get; set; }
 
         [Display(Name = "Entry_NotedAmount")]
